f767e8c234c982b937d4a419b9d6e88d40d12ce8
YounesN <younes.nej2008@gmail.com>
2015-11-12 12:03:19 -0500

    GOMC Serial version 1.00 release

---
+++ src/FFShift.h
@@ -0,0 +1,163 @@
+/*******************************************************************************
+GPU OPTIMIZED MONTE CARLO (GOMC) 1.0 (Serial version)
+Copyright (C) 2015  GOMC Group
+
+A copy of the GNU General Public License can be found in the COPYRIGHT.txt
+along with this program, also can be found at <http://www.gnu.org/licenses/>.
+********************************************************************************/
+#ifndef FF_SHIFT_H
+#define FF_SHIFT_H
+
+#include "EnsemblePreprocessor.h" //For "MIE_INT_ONLY" preprocessor.
+#include "FFConst.h" //constants related to particles.
+#include "../lib/BasicTypes.h" //for uint
+#include "../lib/NumLib.h" //For Cb, Sq
+#include "FFParticle.h"
+
+//////////////////////////////////////////////////////////////////////
+////////////////////////// LJ Shift Style ////////////////////////////
+//////////////////////////////////////////////////////////////////////
+// Virial and LJ potential calculation:
+// U(rij) = cn * eps_ij * ( (sig_ij/rij)^n - (sig_ij/rij)^6) + shiftConst
+// shiftConst = cn * eps_ij * ( (sig_ij/rcut)^n - (sig_ij/rcut)^6)
+// cn = n/(n-6) * ((n/6)^(6/(n-6)))
+//
+// Vir(r) = cn * eps_ij * 6 * ((n/6) * repulse - attract)/rij^2
+// U_lrc = 0
+// Vir_lrc = 0
+
+struct FF_SHIFT : public FFParticle
+{
+ public:
+
+   virtual void CalcAdd(double& en, double& vir, const double distSq,
+                const uint kind1, const uint kind2) const;
+   virtual void CalcSub(double& en, double& vir, const double distSq,
+                const uint kind1, const uint kind2) const;
+   virtual double CalcEn(const double distSq,
+                 const uint kind1, const uint kind2) const;
+   virtual double CalcVir(const double distSq,
+                  const uint kind1, const uint kind2) const;
+   virtual void CalcAdd_1_4(double& en, const double distSq,
+		const uint kind1, const uint kind2) const;
+   //!Returns Ezero, no energy correction
+   virtual double EnergyLRC(const uint kind1, const uint kind2) const
+   {return 0.0;}
+   //!!Returns Ezero, no virial correction
+   virtual double VirialLRC(const uint kind1, const uint kind2) const
+   {return 0.0;}
+
+ private:
+   virtual void Calc(double& en, double& vir, const double distSq, uint index,
+#ifdef MIE_INT_ONLY
+	     const uint n
+#else
+	     const double n
+#endif
+	     ) const;
+
+};
+
+inline void FF_SHIFT::CalcAdd(double& en, double& vir, const double distSq,
+				const uint kind1, const uint kind2) const
+{
+   uint idx = FlatIndex(kind1, kind2);
+   Calc(en, vir, distSq, idx, n[idx]);
+} 
+
+inline void FF_SHIFT::CalcAdd_1_4(double& en, const double distSq,
+		const uint kind1, const uint kind2) const
+{
+   uint index = FlatIndex(kind1, kind2);
+   double rRat2 = sigmaSq_1_4[index]/distSq;
+   double rRat4 = rRat2 * rRat2;
+   double attract = rRat4 * rRat2;
+#ifdef MIE_INT_ONLY
+   uint n_ij = n_1_4[index];
+   double repulse = num::POW(rRat2, rRat4, attract, n_ij);
+#else
+   double n_ij = n_1_4[index];
+   double repulse = pow(sqrt(rRat2), n_ij);
+#endif
+
+   en += (epsilon_cn_1_4[index] * (repulse-attract) - shiftConst_1_4[index]);
+}
+
+inline void FF_SHIFT::CalcSub(double& en, double& vir, const double distSq,
+				const uint kind1, const uint kind2) const
+{
+   double tempEn=0, tempVir=0;
+   uint idx = FlatIndex(kind1, kind2);
+   Calc(tempEn, tempVir, distSq, idx, n[idx]);
+   en -= tempEn;
+   vir = -1.0 * tempVir;
+} 
+
+//mie potential
+inline double FF_SHIFT::CalcEn(const double distSq,
+                                 const uint kind1, const uint kind2) const
+{
+   uint index = FlatIndex(kind1, kind2);
+   double rRat2 = sigmaSq[index]/distSq;
+   double rRat4 = rRat2 * rRat2;
+   double attract = rRat4 * rRat2;
+#ifdef MIE_INT_ONLY
+   uint n_ij = n[index];
+   double repulse = num::POW(rRat2, rRat4, attract, n_ij);
+#else
+   double n_ij = n[index];
+   double repulse = pow(sqrt(rRat2), n_ij);
+#endif
+
+   return (epsilon_cn[index] * (repulse-attract) - shiftConst[index]);
+}
+
+//mie potential
+inline double FF_SHIFT::CalcVir(const double distSq,
+                                  const uint kind1, const uint kind2) const
+{
+   uint index = FlatIndex(kind1, kind2);
+   double rNeg2 = 1.0/distSq;
+   double rRat2 = rNeg2 * sigmaSq[index];
+   double rRat4 = rRat2 * rRat2;
+   double attract = rRat4 * rRat2;
+#ifdef MIE_INT_ONLY
+   uint n_ij = n[index];
+   double repulse = num::POW(rRat2, rRat4, attract, n_ij);
+#else
+   double n_ij = n[index];
+   double repulse = pow(sqrt(rRat2), n_ij);
+#endif
+
+   //Virial is the derivative of the pressure... mu
+   return epsilon_cn_6[index] * (nOver6[index]*repulse-attract)*rNeg2;
+}
+
+
+//mie potential
+inline void FF_SHIFT::Calc(double & en, double & vir, 
+			     const double distSq, const uint index,
+#ifdef MIE_INT_ONLY
+			     const uint n,
+#else
+			     const double n
+#endif
+			     ) const
+{
+   double rNeg2 = 1.0/distSq;
+   double rRat2 = rNeg2 * sigmaSq[index];
+   double rRat4 = rRat2 * rRat2;
+   double attract = rRat4 * rRat2;
+#ifdef MIE_INT_ONLY
+   double repulse = num::POW(rRat2, rRat4, attract, n);
+#else
+   double repulse = pow(sqrt(rRat2), n);
+#endif
+
+   en += (epsilon_cn[index] * (repulse-attract) - shiftConst[index]);
+   //Virial is the derivative of the pressure... mu
+   vir = epsilon_cn_6[index] * (nOver6[index]*repulse-attract)*rNeg2;
+}
+
+#endif /*FF_SHIFT_H*/
+
