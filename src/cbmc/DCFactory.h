163a2971721ae137264048e45aff5e45bf79d996
msoroush <fj2314@wayne.edu>
2015-12-03 11:33:02 -0500

    Add header to CBMC header files

--- src/cbmc/DCFactory.h
+++ src/cbmc/DCFactory.h
@@ -1,3 +1,9 @@
+/*******************************************************************************
+GPU OPTIMIZED MONTE CARLO (GOMC) 1.0 (Serial version)
+Copyright (C) 2015  GOMC Group
+A copy of the GNU General Public License can be found in the COPYRIGHT.txt
+along with this program, also can be found at <http://www.gnu.org/licenses/>.
+********************************************************************************/
 #ifndef DCFACTORY_H
 #define DCFACTORY_H
 
