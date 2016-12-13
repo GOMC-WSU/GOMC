3ee80be45c3f7e5983337b8fa1e9f8c9cff4c346
Younes Nejahi <younes@wayne.edu>
2016-12-12 21:05:03 -0500

    Added header

--- src/FFSwitchMartini.h
+++ src/FFSwitchMartini.h
@@ -1,3 +1,9 @@
+/*******************************************************************************
+GPU OPTIMIZED MONTE CARLO (GOMC) 1.9
+Copyright (C) 2016  GOMC Group
+A copy of the GNU General Public License can be found in the COPYRIGHT.txt
+along with this program, also can be found at <http://www.gnu.org/licenses/>.
+********************************************************************************/
 #ifndef FF_SWITCH_MARTINI_H
 #define FF_SWITCH_MARTINI_H
 
--- src/MoveSettings.cpp
+++ src/MoveSettings.cpp
@@ -1,3 +1,9 @@
+/*******************************************************************************
+GPU OPTIMIZED MONTE CARLO (GOMC) 1.9
+Copyright (C) 2016  GOMC Group
+A copy of the GNU General Public License can be found in the COPYRIGHT.txt
+along with this program, also can be found at <http://www.gnu.org/licenses/>.
+********************************************************************************/
 #include "MoveSettings.h" //header spec.
 #include "BoxDimensions.h" //For axis sizes
 #include "StaticVals.h" //For init info.
