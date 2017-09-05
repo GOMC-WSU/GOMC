82f078e032770f70f5f30bb0e7f371907404eb1a
Younes Nejahi <younes.nej2008@gmail.com>
2017-09-05 16:14:58 -0400

    Fixed few bugs and version number

--- src/GOMC_Config.h
+++
@@ -1,9 +0,0 @@
-/*******************************************************************************
-GPU OPTIMIZED MONTE CARLO (GOMC) 2.1
-Copyright (C) 2016  GOMC Group
-A copy of the GNU General Public License can be found in the COPYRIGHT.txt
-along with this program, also can be found at <http://www.gnu.org/licenses/>.
-********************************************************************************/
-
-#define GOMC_VERSION_MAJOR 2
-#define GOMC_VERSION_MINOR 0
