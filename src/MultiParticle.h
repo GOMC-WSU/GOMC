30a839c18fb9f06fae48733de936a3dc15d93b81
YounesN <younes@wayne.edu>
2018-02-20 13:42:22 -0500

    added coeff

--- src/MultiParticle.h
+++ src/MultiParticle.h
@@ -13,13 +13,22 @@ public:
   virtual void CalcEn();
   virtual uint Transform();
   virtual void Accept(const uint rejectState, const uint step);
+  virtual double GetCoeff();
 private:
   uint bPick;
   SystemPotential sysPotNew;
+  XYZArray& atomForceRef;
+  XYZArray& atomForceNew;
+  XYZArray& atomTorqueRef;
+  XYZArray& atomTorqueNew;
+  Coordinates newMolsPos;
+  COM newCOMs;
 };
 
 inline MultiParticle::MultiParticle(System &sys, StaticVals const &statV) :
-  MoveBase(sys, statV)
+  MoveBase(sys, statV), atomForceRef(sys.atomForcesOld),
+  atomForceNew(sys.atomForcesNew), atomTorqueRef(sys.atomTorqueOld),
+  atomTorqueNew(sys.atomTorqueOld)
 {
 }
 
@@ -34,6 +43,8 @@ inline uint MultiParticle::Transform()
 {
   // Based on the reference force decided whether to displace or rotate each
   // individual particle.
+
+  // move particles according to force and torque and store them in the new pos
   return 0;
 }
 
@@ -41,7 +52,19 @@ inline void MultiParticle::CalcEn()
 {
   // Calculate the new force and energy and we will compare that to the
   // reference values in Accept() function 
+
+  // copy forces to the old one
+  atomForceNew.CopyRange(atomForceRef, 0, 0, atomForceNew.Count());
+  atomTorqueNew.CopyRange(atomTorqueRef, 0, 0, atomTorqueNew.Count());
+  sysPotNew = calcEnRef.BoxInter(sysPotNew, newMolPos, newCOMs, boxDimRef,
+                                 bPick);
+
   return;
+}
+
+inline double MultiParticle::GetCoeff() const
+{
+  // calculate (w_new->old/w_old->new) and return it.
 }
 
 inline void MultiParticle::Accept(const uint rejectState, const uint step)
