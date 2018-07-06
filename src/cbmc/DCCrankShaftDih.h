4f19e51c49c0aa1b7c5ecc639499c0fdee00c65a
msoroush <fj2314@wayne.edu>
2018-07-06 16:11:50 -0400

    Detect atomIndex within two end of dihedral.

---
+++ src/cbmc/DCCrankShaftDih.h
@@ -0,0 +1,40 @@
+/*******************************************************************************
+GPU OPTIMIZED MONTE CARLO (GOMC) 2.31
+Copyright (C) 2018  GOMC Group
+A copy of the GNU General Public License can be found in the COPYRIGHT.txt
+along with this program, also can be found at <http://www.gnu.org/licenses/>.
+********************************************************************************/
+#ifndef DCSINGLE_H
+#define DCSINGLE_H
+
+#include "DCComponent.h"
+#include "BasicTypes.h"
+
+namespace mol_setup
+{
+class MolKind;
+}
+
+namespace cbmc
+{
+class DCData;
+
+class DCSingle : public DCComponent
+{
+public:
+  DCSingle(DCData* data, uint atom);
+  void PrepareNew(TrialMol& newMol, uint molIndex) {};
+  void PrepareOld(TrialMol& oldMol, uint molIndex) {};
+  void BuildOld(TrialMol& oldMol, uint molIndex);
+  void BuildNew(TrialMol& newMol, uint molIndex);
+  DCComponent* Clone()
+  {
+    return new DCSingle(*this);
+  }
+
+private:
+  DCData* data;
+  uint atom;
+};
+}
+#endif
