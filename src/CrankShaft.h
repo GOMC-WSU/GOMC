28868e8fecdb899c5b8e1ca268edfcb7d8ef9358
msoroush <fj2314@wayne.edu>
2019-01-23 14:37:27 -0500

    Fix to the error in calculating correction term of Ewald in Crankshaft move.

--- src/CrankShaft.h
+++ src/CrankShaft.h
@@ -125,7 +125,7 @@ inline void CrankShaft::Accept(const uint rejectState, const uint step)
     double Wrat = Wn / Wo * W_recip;
 
     //safety to make sure move will be rejected in overlap case
-    if(!overlap) {
+    if(newMol.GetWeight() != 0.0 && !overlap) {
       result = prng() < Wrat;
     } else
       result = false;
