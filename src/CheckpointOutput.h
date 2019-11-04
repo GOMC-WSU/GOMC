3da9cfc8901da04e8d99ea6fbfcf9b2559f31662
Greg Schwing <gschwing@uno.edu>
2019-11-04 16:45:55 -0500

    Included added to Checkpoint

--- src/CheckpointOutput.h
+++ src/CheckpointOutput.h
@@ -11,6 +11,7 @@ along with this program, also can be found at <http://www.gnu.org/licenses/>.
 #include "Coordinates.h"
 #include "MoveBase.h"
 #include <iostream>
+#include "GOMC_Config.h"
 
 class CheckpointOutput : public OutputableBase
 {
