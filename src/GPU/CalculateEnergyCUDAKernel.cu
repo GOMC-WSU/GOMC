cbbeb546ea97e7674c7a6cac81e5c10fa1942a5f
Younes Nejahi <younes@wayne.edu>
2019-12-23 21:11:34 -0800

    wrong function call in CalcEnSwitchGPU

--- src/GPU/CalculateEnergyCUDAKernel.cu
+++ src/GPU/CalculateEnergyCUDAKernel.cu
@@ -668,8 +668,9 @@ __device__ double CalcEnSwitchGPU(double distSq, int index, double *gpu_sigmaSq,
   double softDist6 = lambdaCoef * sigma6 + dist6;
   double softRsq = pow(softDist6, (double)1.0/3.0);
 
-  return gpu_lambdaVDW * CalcEn(softRsq, index, gpu_sigmaSq, gpu_n,
-                                gpu_epsilon_Cn, gpu_rCut, gpu_rOn);
+  return gpu_lambdaVDW * CalcEnSwitchGPUNoLambda(softRsq, index, gpu_sigmaSq,
+                                                 gpu_n, gpu_epsilon_Cn,
+                                                 gpu_rCut, gpu_rOn);
 }
 
 __device__ double CalcEnSwitchGPUNoLambda(double distSq, int index,
