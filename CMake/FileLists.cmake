ede043cc45dcb885e1221a0dc86df6de3420eb9c
msoroush <fj2314@wayne.edu>
2020-02-22 13:28:07 -0600

    Start working on multiparticle in spirit of brownian motion

--- CMake/FileLists.cmake
+++ CMake/FileLists.cmake
@@ -174,6 +174,7 @@ set(headers
    src/moves/MoleculeTransfer.h
    src/moves/MoveBase.h
    src/moves/MultiParticle.h
+   src/moves/MultiParticleBrownianMotion.h
    src/moves/Regrowth.h
    src/moves/Rotation.h
    src/moves/TargetedSwap.h
