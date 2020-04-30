a56a6aa970b823640c0818cd989a68b8f092e0ad
Younes Nejahi <younes@wayne.edu>
2020-04-30 00:43:05 -0700

    put back the intel compiler flag in metamake.sh

--- metamake.sh
+++ metamake.sh
@@ -50,9 +50,9 @@ fi
 
 mkdir -p bin
 cd bin
-#ICC_PATH="$(which icc)"
-#ICPC_PATH="$(which icpc)"
-#export CC=${ICC_PATH}
-#export CXX=${ICPC_PATH}
+ICC_PATH="$(which icc)"
+ICPC_PATH="$(which icpc)"
+export CC=${ICC_PATH}
+export CXX=${ICPC_PATH}
 cmake ..
 make
