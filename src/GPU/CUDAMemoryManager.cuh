1a90349b30e0c6ccb02b17037e4d051f48ae3fc5
Younes Nejahi <younes@wayne.edu>
2020-05-30 12:38:56 -0700

    make isFreed() function public

--- src/GPU/CUDAMemoryManager.cuh
+++ src/GPU/CUDAMemoryManager.cuh
@@ -9,11 +9,11 @@ class CUDAMemoryManager {
 public:
   static cudaError_t mallocMemory(void **address, unsigned int size);
   static cudaError_t freeMemory(void *address);
+  static bool isFreed();
 
 private:
   static long long totalAllocatedBytes;
   static std::unordered_map<void *, unsigned int> allocatedPointers;
-  static bool isFreed();
 };
 
 cudaError_t CUDAMemoryManager::mallocMemory(void **address, unsigned int size) {
