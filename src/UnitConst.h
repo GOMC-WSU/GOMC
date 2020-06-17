a0f30e19a59804998ada010ea327ae9010c3a736
Younes Nejahi <younes@wayne.edu>
2020-06-16 20:36:55 -0700

    Change version number of header sections in each file

--- src/UnitConst.h
+++ src/UnitConst.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
-GPU OPTIMIZED MONTE CARLO (GOMC) 2.51
+GPU OPTIMIZED MONTE CARLO (GOMC) 2.60
 Copyright (C) 2018  GOMC Group
 A copy of the GNU General Public License can be found in the COPYRIGHT.txt
 along with this program, also can be found at <http://www.gnu.org/licenses/>.
