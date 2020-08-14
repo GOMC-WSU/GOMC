8088354b0a83554598eb639792dc8a97b3837101
LSchwiebert <loren@wayne.edu>
2020-08-13 22:09:54 -0400

    Fixed #202 Memory leak/memory access issues when checkpointing

--- src/CheckpointOutput.cpp
+++ src/CheckpointOutput.cpp
@@ -85,7 +85,10 @@ void CheckpointOutput::printRandomNumbers()
   // there is a save function inside MersenneTwister.h file
   // to read back we can use the load function
   const int N = 624;
-  uint32_t* saveArray = new uint32_t[N];
+  // The MT::save function also appends the "left" variable,
+  // so need to allocate one more array element
+  uint32_t* saveArray = new uint32_t[N+1];
+  saveArray[624] = -123456;
   prngRef.GetGenerator()->save(saveArray);
   for(int i = 0; i < N; i++) {
     outputUintIn8Chars(saveArray[i]);
@@ -102,6 +105,8 @@ void CheckpointOutput::printRandomNumbers()
   // let's save seedValue just in case
   // not sure if that is used or not, or how important it is
   outputUintIn8Chars(prngRef.GetGenerator()->seedValue);
+  
+  delete[] saveArray;
 }
 
 void CheckpointOutput::printCoordinates()
@@ -207,7 +212,7 @@ void CheckpointOutput::printVector2DUint(std::vector< std::vector< uint > > data
   outputUintIn8Chars(size_x);
   outputUintIn8Chars(size_y);
 
-  // print array iteself
+  // print array itself
   for(int i = 0; i < size_x; i++) {
     for(int j = 0; j < size_y; j++) {
       outputUintIn8Chars(data[i][j]);
