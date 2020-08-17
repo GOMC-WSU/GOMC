b15077a7a2d918122d6e9bbca61a0b844d443d73
go2432 <go2432@wayne.edu>
2020-08-17 11:34:29 -0400

    added std in front of vector

--- src/ParallelTemperingUtilities.h
+++ src/ParallelTemperingUtilities.h
@@ -71,8 +71,8 @@ public:
 explicit ParallelTemperingUtilities(MultiSim const*const& multisim, System & sys, StaticVals const& statV, ulong parallelTempFreq, ulong parallelTemperingAttemptsPerExchange);
 void evaluateExchangeCriteria(ulong step);
 void prepareToDoExchange(const int replica_id, int* maxswap, bool* bThisReplicaExchanged);
-void cyclicDecomposition(const vector<int> destinations, vector< vector<int> > & cyclic, vector<bool> & incycle, const int nrepl, int * nswap);
-void computeExchangeOrder(vector< vector<int> > & cyclic, vector< vector<int> > & order, const int nrepl, const int maxswap);
+void cyclicDecomposition(const std::vector<int> destinations, std::vector< std::vector<int> > & cyclic, std::vector<bool> & incycle, const int nrepl, int * nswap);
+void computeExchangeOrder(std::vector< std::vector<int> > & cyclic, std::vector< std::vector<int> > & order, const int nrepl, const int maxswap);
 
 void conductExchanges(Coordinates & coordCurrRef, COM & comCurrRef, MultiSim const*const& ms, const int & maxSwap, const bool & bThisReplicaExchanged);
 void exchangePositionsNonBlocking(Coordinates & myPos, MultiSim const*const& multisim, int exchangePartner);
@@ -83,12 +83,12 @@ void exchangeCOMs(COM & myCOMs, MultiSim const*const& multisim, int exchangePart
 void exchangeCellLists(CellList & myCellList, MultiSim const*const& multisim, int exchangePartner, bool leader);
 void exchangePotentials(SystemPotential & mySystemPotential, MultiSim const*const& multisim, int exchangePartner, bool leader);
 void exchangeVirials(SystemPotential & mySystemPotential, MultiSim const*const& multisim, int exchangePartner, bool leader);
-void print_ind(FILE * fplog, const char* leg, int n, vector<int> ind, const vector<bool> bEx);
-void print_prob(FILE * fplog, const char* leg, int n, vector<double> prob);
-void print_count(FILE * fplog, const char* leg, int n, vector<int> count);
-void print_transition_matrix(FILE * fplog, int n, vector< vector<int> > nmoves, const vector<int> nattempt);
+void print_ind(FILE * fplog, const char* leg, int n, std::vector<int> ind, const std::vector<bool> bEx);
+void print_prob(FILE * fplog, const char* leg, int n, std::vector<double> prob);
+void print_count(FILE * fplog, const char* leg, int n, std::vector<int> count);
+void print_transition_matrix(FILE * fplog, int n, std::vector< std::vector<int> > nmoves, const std::vector<int> nattempt);
 void print_replica_exchange_statistics(FILE * fplog);
-void print_allswitchind(FILE* fplog, int n, vector<int> pind, vector<int> allswaps, vector<int> tmpswap);
+void print_allswitchind(FILE* fplog, int n, std::vector<int> pind, std::vector<int> allswaps, std::vector<int> tmpswap);
 #endif
 
 private:
@@ -100,35 +100,35 @@ PRNG & prng;
 SystemPotential & sysPotRef;
 SystemPotential sysPotNew;
 ulong parallelTempFreq, parallelTemperingAttemptsPerExchange;
-vector<double> global_betas;
-vector<int> ind, pind;
-vector<bool> exchangeResults;
-vector<double> exchangeProbabilities;
-vector<int> nexchange;
-vector<int> nattempt;
+std::vector<double> global_betas;
+std::vector<int> ind, pind;
+std::vector<bool> exchangeResults;
+std::vector<double> exchangeProbabilities;
+std::vector<int> nexchange;
+std::vector<int> nattempt;
 
 /* For multiex */
-vector<int> allswaps;
-vector<int> tempswap;
-vector< vector<int> > cyclic;
-vector< vector<int> > order;
-vector<bool> incycle;
+std::vector<int> allswaps;
+std::vector<int> tempswap;
+std::vector< std::vector<int> > cyclic;
+std::vector< std::vector<int> > order;
+std::vector<bool> incycle;
 /* For multiex */
 
 
 
 //! Sum of probabilities
-vector<double> prob_sum;
+std::vector<double> prob_sum;
 //! Number of moves between replicas i and j
-vector< vector<int> > nmoves;
+std::vector< std::vector<int> > nmoves;
 
 Coordinates newMolsPos;
 COM newCOMs;
 
 #if BOX_TOTAL == 1    
-    vector<double> global_energies;
+    std::vector<double> global_energies;
 #else
-    vector<vector <double> > global_energies;
+    std::vector<std::vector <double> > global_energies;
 #endif
 
 };
