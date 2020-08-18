6691945606a5f6b63b8d78260a16de32f8c1f982
Younes Nejahi <younes@wayne.edu>
2020-08-18 10:11:38 -0700

    replace shared memory with register for now

--- src/GPU/CalculateEwaldCUDAKernel.cu
+++ src/GPU/CalculateEwaldCUDAKernel.cu
@@ -474,11 +474,13 @@ __global__ void BoxForceReciprocalGPU(
   double lambdaCoef = DeviceGetLambdaCoulomb(moleculeID, kindID, box, gpu_isFraction, gpu_molIndex, gpu_kindIndex, gpu_lambdaCoulomb);
   // loop over images
   for(int vectorIndex = 0; vectorIndex < IMAGES_PER_BLOCK; vectorIndex ++) {
-    double dot = x * shared_kvector[vectorIndex*6] + y *
-      shared_kvector[vectorIndex*6+1] + z * shared_kvector[vectorIndex*6+2];
-    double factor = (2.0 * gpu_particleCharge[particleID] * shared_kvector[vectorIndex*6+3] * lambdaCoef) * 
-      (sin(dot) * shared_kvector[vectorIndex*6+4] - cos(dot) * shared_kvector[vectorIndex*6+5]);
-      
+    double dot = x * vectorIndex + y * vectorIndex + z * vectorIndex;
+    double factor = (2.0 * vectorIndex * vectorIndex * lambdaCoef) * (sin(dot) * vectorIndex - cos(dot) * vectorIndex);
+    // double dot = x * shared_kvector[vectorIndex*6] + y *
+    //   shared_kvector[vectorIndex*6+1] + z * shared_kvector[vectorIndex*6+2];
+    // double factor = (2.0 * gpu_particleCharge[particleID] * shared_kvector[vectorIndex*6+3] * lambdaCoef) * 
+    //   (sin(dot) * shared_kvector[vectorIndex*6+4] - cos(dot) * shared_kvector[vectorIndex*6+5]);
+    
     forceX += factor * shared_kvector[vectorIndex*6];
     forceY += factor * shared_kvector[vectorIndex*6+1];
     forceZ += factor * shared_kvector[vectorIndex*6+2];
