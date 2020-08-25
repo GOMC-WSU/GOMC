2336af2dd2a5d43d87dc254c41dc2978aaa4dac3
go2432 <go2432@wayne.edu>
2020-08-25 00:31:45 -0400

    Debugging nvcc flags, keep asm flag, and turn resolve cuda symbols property on.  This allows developers to use cuda-gdb and the Eclipse w EE Plugins/NSight VSStudio visual debuggers.

--- CMake/GOMCCUDASetup.cmake
+++ CMake/GOMCCUDASetup.cmake
@@ -1,7 +1,15 @@
 # Find CUDA is enabled, set it up
 
 set(CUDA_NVCC_FLAGS ${CUDA_NVCC_FLAGS};-DGOMC_CUDA)
+######## For CUDA Debugging in Eclipse/cuda-gdb
+if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+	message("-- Debug build type detected, passing : '-g -G --keep' to nvcc")
+	set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -g -G --keep")
+endif()
+######## For CUDA Debugging in Eclipse/cuda-gdb
 set(CUDA_NVCC_FLAGS "${CUDA_NVCC_FLAGS} -std=c++11")
+
+
 include_directories(src/GPU)
 #set(CUDA_VERBOSE_BUILD ON)
 set(CUDA_SEPARABLE_COMPILATION ON)
@@ -22,6 +30,12 @@ if(ENSEMBLE_GPU_NVT)
     set_target_properties(GPU_NVT PROPERTIES
         OUTPUT_NAME ${GPU_NVT_name}
         COMPILE_FLAGS "${GPU_NVT_flags}")
+######## For CUDA Debugging in Eclipse/cuda-gdb
+	if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+		message("-- Debug build type detected, GPU_NVT setting CUDA_RESOLVE_DEVICE_SYMBOLS ON")
+    	set_property(TARGET GPU_NVT PROPERTY CUDA_RESOLVE_DEVICE_SYMBOLS ON)
+	endif()
+######## For CUDA Debugging in Eclipse/cuda-gdb
     if(WIN32)
         #needed for hostname
         target_link_libraries(GPU_NVT ws2_32)
@@ -29,6 +43,7 @@ if(ENSEMBLE_GPU_NVT)
     if(MPI_FOUND)
 	target_link_libraries(GPU_NVT ${MPI_LIBRARIES})
     endif()
+    
 endif()
 
 if(ENSEMBLE_GPU_GEMC)
@@ -37,6 +52,12 @@ if(ENSEMBLE_GPU_GEMC)
     set_target_properties(GPU_GEMC PROPERTIES
         OUTPUT_NAME ${GPU_GE_name}
         COMPILE_FLAGS "${GPU_GE_flags}")
+######## For CUDA Debugging in Eclipse/cuda-gdb
+	if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+		message("-- Debug build type detected, GPU_GEMC setting CUDA_RESOLVE_DEVICE_SYMBOLS ON")
+    	set_property(TARGET GPU_GEMC PROPERTY CUDA_RESOLVE_DEVICE_SYMBOLS ON)
+	endif()
+######## For CUDA Debugging in Eclipse/cuda-gdb
     if(WIN32)
         #needed for hostname
         target_link_libraries(GPU_GEMC ws2_32)
@@ -52,6 +73,12 @@ if(ENSEMBLE_GPU_GCMC)
     set_target_properties(GPU_GCMC PROPERTIES
         OUTPUT_NAME ${GPU_GC_name}
         COMPILE_FLAGS "${GPU_GC_flags}")
+######## For CUDA Debugging in Eclipse/cuda-gdb
+	if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+		message("-- Debug build type detected, GPU_GCMC setting CUDA_RESOLVE_DEVICE_SYMBOLS ON")
+    	set_property(TARGET GPU_GCMC PROPERTY CUDA_RESOLVE_DEVICE_SYMBOLS ON)
+	endif()
+######## For CUDA Debugging in Eclipse/cuda-gdb
     if(WIN32)
         #needed for hostname
         target_link_libraries(GPU_GCMC ws2_32)
@@ -67,6 +94,12 @@ if(ENSEMBLE_GPU_NPT)
     set_target_properties(GPU_NPT PROPERTIES
         OUTPUT_NAME ${GPU_NPT_name}
         COMPILE_FLAGS "${GPU_NPT_flags}")
+######## For CUDA Debugging in Eclipse/cuda-gdb
+	if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+		message("-- Debug build type detected, GPU_NPT setting CUDA_RESOLVE_DEVICE_SYMBOLS ON")
+    	set_property(TARGET GPU_NPT PROPERTY CUDA_RESOLVE_DEVICE_SYMBOLS ON)
+	endif()
+######## For CUDA Debugging in Eclipse/cuda-gdb
     if(WIN32)
         #needed for hostname
         target_link_libraries(GPU_NPT ws2_32)
