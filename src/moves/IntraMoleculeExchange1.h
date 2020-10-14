33288ab28f7f352e264bcf70503b95c46dc6d448
Younes Nejahi <younesnejahi@gmail.com>
2020-10-14 15:51:09 -0700

    Update IntraMoleculeExchange1.h

--- src/moves/IntraMoleculeExchange1.h
+++ src/moves/IntraMoleculeExchange1.h
@@ -1,3 +1,9 @@
+/*******************************************************************************
+GPU OPTIMIZED MONTE CARLO (GOMC) 2.70
+Copyright (C) 2018  GOMC Group
+A copy of the GNU General Public License can be found in the COPYRIGHT.txt
+along with this program, also can be found at <http://www.gnu.org/licenses/>.
+********************************************************************************/
 #ifndef INTRAMOLECULEEXCHANGE1_H
 #define INTRAMOLECULEEXCHANGE1_H
 
