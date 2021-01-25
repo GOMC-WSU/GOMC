6a396413b38f36b4c3de99cf3c07c48de81a9c53
GregorySchwing <gschwing@uno.edu>
2021-01-25 14:42:01 -0500

    Added tripeptide with 3 simple cycles test case

--- test/FileList.cmake
+++ test/FileList.cmake
@@ -2,6 +2,37 @@ set(TestSources
     test/src/BasicTypesTest.cpp
     test/src/BitLibTest.cpp
     test/src/EndianTest.cpp
-    test/src/PSFParserTest.cpp
     test/src/MolLookupTest.cpp
+    test/src/CircuitTester.cpp
+    test/src/PSFParserTest.cpp
+)
+
+set(TestHeaders
+
+)
+
+set(GOMCSources
+    src/PDBSetup.cpp
+    src/MolSetup.cpp
+    src/BondAdjacencyList.cpp
+    src/ConfigSetup.cpp
+    src/FFSetup.cpp
+    src/FFConst.cpp
+    src/Reader.cpp
+    lib/CircuitFinder.cpp
+    src/InputFileReader.cpp
+    lib/FloydWarshallCycle.cpp
+)
+
+set(GOMCHeaders
+    src/PDBSetup.h
+    src/MolSetup.h
+    src/BondAdjacencyList.h
+    src/ConfigSetup.h
+    src/FFSetup.h
+    src/FFConst.h
+    src/Reader.h
+    lib/CircuitFinder.h
+    src/InputFileReader.h
+    lib/FloydWarshallCycle.h
 )
