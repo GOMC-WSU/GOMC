74937c21517d59f4e0e8113b3b96f7402f72d4ad
GregorySchwing <gschwing@uno.edu>
2021-05-10 14:28:00 -0500

    writing test

---
+++ test/src/ConsistentTrajectoryTest.cpp
@@ -0,0 +1,42 @@
+#include <gtest/gtest.h>
+#include "PDBSetup.h"
+#include "MolSetup.h"
+#include "BondAdjacencyList.h"
+#include "ConfigSetup.h"
+#include "FFSetup.h"        //For geometry kinds
+#include "FFConst.h"
+#include "Reader.h"
+#include "InputFileReader.h"
+
+
+TEST(ConsistentTrajectoryTest, CheckCheckpointVersusPSF) {
+
+    config_setup::RestartSettings rs;
+    std::string pdbnames[2];
+
+    pdbnames[0] = "./test/input/ConsistentTrajectory/BOX_0.pdb";
+    pdbnames[1] = "./test/input/ConsistentTrajectory/BOX_1.pdb";
+
+    PDBSetup pdb;
+    config_setup::InFiles if2;
+
+    pdb.Init(rs, if2, pdbnames);
+
+    std::string psfnames[2];
+    bool psfdefined[2];
+
+    MolSetup ms;
+
+    /*
+        Let GOMC parse dialanine
+    */
+
+    psfnames[0] = "./test/input/ConsistentTrajectory/BOX_0.psf";
+    psfnames[1] = "./test/input/ConsistentTrajectory/BOX_1.psf";
+
+    psfdefined[0] = true;
+    psfdefined[1] = true;
+
+    ms.Init(psfnames, psfdefined, pdb.atoms);
+
+}
