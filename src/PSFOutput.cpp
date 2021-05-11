873329e9549b4c71c83754c0423f84a4a49da5f5
GregorySchwing <gschwing@uno.edu>
2021-05-11 17:36:10 -0500

    needed a c_str

--- src/PSFOutput.cpp
+++ src/PSFOutput.cpp
@@ -244,11 +244,11 @@ void PSFOutput::PrintAtoms(FILE* outfile) const
         //atom name, atom type, charge, mass, and an unused 0
 
         if(molKinds[thisKIndex].isMultiResidue){
-            fprintf(outfile, atomFormat, atomID, moleculeSegmentNames[mI],
+            fprintf(outfile, atomFormat, atomID, moleculeSegmentNames[mI].c_str(),
                     resID + molKinds[thisKIndex].intraMoleculeResIDs[at], thisAtom->residue.c_str(), thisAtom->name.c_str(),
                     thisAtom->type.c_str(), thisAtom->charge, thisAtom->mass, 0);
           } else {
-            fprintf(outfile, atomFormat, atomID, moleculeSegmentNames[mI],
+            fprintf(outfile, atomFormat, atomID, moleculeSegmentNames[mI].c_str(),
                     resID, thisAtom->residue.c_str(), thisAtom->name.c_str(),
                     thisAtom->type.c_str(), thisAtom->charge, thisAtom->mass, 0);
           }
@@ -387,13 +387,13 @@ void PSFOutput::PrintDihedrals(FILE* outfile) const
 
         if(molKinds[thisKind].isMultiResidue){
           fprintf(outfile, atomFormat, atomID, 
-                  moleculeSegmentNames[*thisMol],
+                  moleculeSegmentNames[*thisMol].c_str(),
                   resID + molKinds[thisKind].intraMoleculeResIDs[at], 
                   thisAtom->residue.c_str(), thisAtom->name.c_str(),
                   thisAtom->type.c_str(), thisAtom->charge, thisAtom->mass, 0);
         } else {
           fprintf(outfile, atomFormat, atomID, 
-                  moleculeSegmentNames[*thisMol],
+                  moleculeSegmentNames[*thisMol].c_str(),
                   resID, thisAtom->residue.c_str(), thisAtom->name.c_str(),
                   thisAtom->type.c_str(), thisAtom->charge, thisAtom->mass, 0);
         }
