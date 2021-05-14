e07786528ae266737f71b77b8aecb799f2e3e792
GregorySchwing <gschwing@uno.edu>
2021-05-14 16:19:52 -0500

    fixed test for chain letter

--- test/src/ConsistentTrajectoryTest.cpp
+++ test/src/ConsistentTrajectoryTest.cpp
@@ -10,46 +10,52 @@
 #include "AlphaNum.h"
 #include "CheckpointSetup.h"
 
+/* There are 4 cases for restarting from checkpoint 
 
-TEST(ConsistentTrajectoryTest, CheckCheckpointVersusPSF) {
+1) Base Case:
+    The first run.  
+    Checkpoint Setup  - RestartFromCheckpoint = false
+    Checkpoint Output - RestartFromCheckpoint = false
+
+2) The first restart from checkpoint
+    Checkpoint Setup  - RestartFromCheckpoint = true
+    Checkpoint Output - RestartFromCheckpoint = true
+
+3) The Nth restart from checkpoint from a prev checkpoint
+    Checkpoint Setup  - RestartFromCheckpoint = true
+    Checkpoint Output - RestartFromCheckpoint = true
+
+*/
+
+TEST(ConsistentTrajectoryTest, CheckChainLetters) {
 
     config_setup::RestartSettings rs;
-    std::string pdbnames[2];
-
-    pdbnames[0] = "./test/input/ConsistentTrajectory/BOX_0.pdb";
-    pdbnames[1] = "./test/input/ConsistentTrajectory/BOX_1.pdb";
-
-    PDBSetup pdb;
     config_setup::InFiles if2;
 
-    pdb.Init(rs, if2, pdbnames);
+    std::string pdbnamesSTART[2], pdbnamesBase[2], pdbnames1[2], pdbnamesN[2], pdbnamesNPlus1[2];
 
-    std::string psfnames[2];
-    bool psfdefined[2];
+    pdbnamesSTART[0] = "./test/input/ConsistentTrajectory/START_BOX_0.pdb";
+    pdbnamesSTART[1] = "./test/input/ConsistentTrajectory/START_BOX_1.pdb";
 
-    MolSetup ms;
+    pdbnamesBase[0] = "./test/input/ConsistentTrajectory/base_BOX_0.pdb";
+    pdbnamesBase[1] = "./test/input/ConsistentTrajectory/base_BOX_1.pdb";
 
-    /*
-        Let GOMC parse dialanine
-    */
+    pdbnames1[0] = "./test/input/ConsistentTrajectory/1_BOX_0.pdb";
+    pdbnames1[1] = "./test/input/ConsistentTrajectory/1_BOX_1.pdb";
 
-    psfnames[0] = "./test/input/ConsistentTrajectory/BOX_0.psf";
-    psfnames[1] = "./test/input/ConsistentTrajectory/BOX_1.psf";
+    pdbnamesN[0] = "./test/input/ConsistentTrajectory/n_BOX_0.pdb";
+    pdbnamesN[1] = "./test/input/ConsistentTrajectory/n_BOX_1.pdb";
 
-    psfdefined[0] = true;
-    psfdefined[1] = true;
+    PDBSetup pdbStart, pdbBase, pdb1, pdbN;
 
-    ms.Init(psfnames, psfdefined, pdb.atoms);
-
-    CheckpointSetup csp("./test/input/ConsistentTrajectory/Checkpoint.chk");
-    csp.ReadAll();
-
+    pdbStart.Init(rs, if2, pdbnamesSTART);
+    pdbBase.Init(rs, if2, pdbnamesBase);    
+    pdb1.Init(rs, if2, pdbnames1);
+    pdbN.Init(rs, if2, pdbnamesN);
     
-    typedef std::vector<std::string>::const_iterator segmentNameIterator;
-    typedef std::vector<uint>::const_iterator checkpointIterator;
-    std::pair<segmentNameIterator, checkpointIterator> itPair(ms.molVars.moleculeSegmentNames.cbegin(), csp.originalMoleculeIndicesVec.cbegin());
-    AlphaNum seg2Uint;
-    for (; itPair.second != csp.originalMoleculeIndicesVec.cend(); ++itPair.first, ++itPair.second){
-        EXPECT_EQ(seg2Uint.string2Uint(*itPair.first) == *itPair.second, true);
+    for (uint i = 0; i < pdbStart.atoms.count; ++i){
+        EXPECT_EQ(pdbStart.atoms.chainLetter[i] == pdbBase.atoms.chainLetter[i], true);
+        EXPECT_EQ(pdbStart.atoms.chainLetter[i] == pdb1.atoms.chainLetter[i], true);
+        EXPECT_EQ(pdbStart.atoms.chainLetter[i] == pdbN.atoms.chainLetter[i], true);
     }
 }
