156b58213f13a7612cfb79fa395bd6419bbc7af2
GregorySchwing <gschwing@uno.edu>
2021-05-15 14:17:34 -0500

    wrapping up dcd gtest

--- test/src/ConsistentTrajectoryTest.cpp
+++ test/src/ConsistentTrajectoryTest.cpp
@@ -132,7 +132,7 @@ TEST(ConsistentTrajectoryTest, CheckPDBTrajCoordinates) {
     pdb1Restart.Init(rs1Restart, if2, pdbnames1Restart);
     pdbNRestart.Init(rsNRestart, if2, pdbnamesNRestart);
     
-    for (uint i = 0; i < pdbStart.atoms.count; ++i){
+    for (uint i = 0; i < pdbBase.atoms.count; ++i){
         /* Find mol i's chain index in restart output files */
         ptrdiff_t pos = find(pdbBaseRestart.atoms.chainLetter.begin(), 
             pdbBaseRestart.atoms.chainLetter.end(), pdbBase.atoms.chainLetter[i])
@@ -142,7 +142,7 @@ TEST(ConsistentTrajectoryTest, CheckPDBTrajCoordinates) {
         EXPECT_EQ(pdbBase.atoms.x[i] == pdbBaseRestart.atoms.x[pos], true);
     }
 
-    for (uint i = 0; i < pdbStart.atoms.count; ++i){
+    for (uint i = 0; i < pdb1.atoms.count; ++i){
         /* Find mol i's chain index in restart output files */
         ptrdiff_t pos = find(pdb1Restart.atoms.chainLetter.begin(), 
             pdb1Restart.atoms.chainLetter.end(), pdb1.atoms.chainLetter[i])
@@ -152,7 +152,7 @@ TEST(ConsistentTrajectoryTest, CheckPDBTrajCoordinates) {
         EXPECT_EQ(pdb1.atoms.x[i] == pdb1Restart.atoms.x[pos], true);
     }
 
-    for (uint i = 0; i < pdbStart.atoms.count; ++i){
+    for (uint i = 0; i < pdbN.atoms.count; ++i){
         /* Find mol i's chain index in restart output files */
         ptrdiff_t pos = find(pdbNRestart.atoms.chainLetter.begin(), 
             pdbNRestart.atoms.chainLetter.end(), pdbN.atoms.chainLetter[i])
@@ -171,36 +171,50 @@ TEST(ConsistentTrajectoryTest, CheckDCDTrajCoordinates) {
     config_setup::RestartSettings rs1;
     config_setup::RestartSettings rsN;
 
-    config_setup::InFiles ifNull;
+    config_setup::InFiles ifBaseRestart;
+    config_setup::InFiles if1Restart;
+    config_setup::InFiles ifNRestart;
 
     config_setup::InFiles ifBase;
     config_setup::InFiles if1;
     config_setup::InFiles ifN;
 
     ifBase.binaryTrajectory.name[0] = "./test/input/ConsistentTrajectory/base_BOX_0.dcd";
+    ifBase.binaryTrajectory.defined[0] = true;
     ifBase.binaryTrajectory.name[1] = "./test/input/ConsistentTrajectory/base_BOX_1.dcd";
+    ifBase.binaryTrajectory.defined[1] = true;
 
     if1.binaryTrajectory.name[0] = "./test/input/ConsistentTrajectory/1_BOX_0.dcd";
+    if1.binaryTrajectory.defined[0] = true;
     if1.binaryTrajectory.name[1] = "./test/input/ConsistentTrajectory/1_BOX_1.dcd";
+    if1.binaryTrajectory.defined[1] = true;
 
     ifN.binaryTrajectory.name[0] = "./test/input/ConsistentTrajectory/n_BOX_0.dcd";
+    ifN.binaryTrajectory.defined[0] = true;
     ifN.binaryTrajectory.name[1] = "./test/input/ConsistentTrajectory/n_BOX_1.dcd";
+    ifN.binaryTrajectory.defined[1] = true;
+
+    ifBaseRestart.binaryCoorInput.name[0] = "./test/input/ConsistentTrajectory/base_BOX_0_restart.coor";
+    ifBaseRestart.binaryCoorInput.defined[0] = true;
+    ifBaseRestart.binaryCoorInput.name[0] = "./test/input/ConsistentTrajectory/base_BOX_1_restart.coor";
+    ifBaseRestart.binaryCoorInput.defined[0] = true;
+
+    if1Restart.binaryCoorInput.name[0] = "./test/input/ConsistentTrajectory/1_BOX_0_restart.coor";
+    if1Restart.binaryCoorInput.defined[0] = true;
+    if1Restart.binaryCoorInput.name[0] = "./test/input/ConsistentTrajectory/1_BOX_1_restart.coor";
+    if1Restart.binaryCoorInput.defined[0] = true;
+
+    ifNRestart.binaryCoorInput.name[0] = "./test/input/ConsistentTrajectory/n_BOX_0_restart.coor";
+    ifNRestart.binaryCoorInput.defined[0] = true;
+    ifNRestart.binaryCoorInput.name[0] = "./test/input/ConsistentTrajectory/n_BOX_1_restart.coor";
+    ifNRestart.binaryCoorInput.defined[0] = true;
 
     std::string pdbnamesSTART[2], pdbnamesBase[2], pdbnames1[2], pdbnamesN[2];
     std::string pdbnamesBaseRestart[2], pdbnames1Restart[2], pdbnamesNRestart[2];
 
     pdbnamesSTART[0] = "./test/input/ConsistentTrajectory/START_BOX_0.pdb";
     pdbnamesSTART[1] = "./test/input/ConsistentTrajectory/START_BOX_1.pdb";
-/*
-    pdbnamesBase[0] = "./test/input/ConsistentTrajectory/base_BOX_0.pdb";
-    pdbnamesBase[1] = "./test/input/ConsistentTrajectory/base_BOX_1.pdb";
 
-    pdbnames1[0] = "./test/input/ConsistentTrajectory/1_BOX_0.pdb";
-    pdbnames1[1] = "./test/input/ConsistentTrajectory/1_BOX_1.pdb";
-
-    pdbnamesN[0] = "./test/input/ConsistentTrajectory/n_BOX_0.pdb";
-    pdbnamesN[1] = "./test/input/ConsistentTrajectory/n_BOX_1.pdb";
-*/
     pdbnamesBaseRestart[0] = "./test/input/ConsistentTrajectory/base_BOX_0_restart.pdb";
     pdbnamesBaseRestart[1] = "./test/input/ConsistentTrajectory/base_BOX_1_restart.pdb";
 
@@ -209,8 +223,6 @@ TEST(ConsistentTrajectoryTest, CheckDCDTrajCoordinates) {
 
     pdbnamesNRestart[0] = "./test/input/ConsistentTrajectory/n_BOX_0_restart.pdb";
     pdbnamesNRestart[1] = "./test/input/ConsistentTrajectory/n_BOX_1_restart.pdb";
-
-
 
     PDBSetup pdbStart, pdbBase, pdb1, pdbN;
     PDBSetup pdbBaseRestart, pdb1Restart, pdbNRestart;
@@ -229,12 +241,6 @@ TEST(ConsistentTrajectoryTest, CheckDCDTrajCoordinates) {
     pdb1.Init(rs1, if1, pdbnamesBaseRestart, frameNum);
     pdbN.Init(rsN, ifN, pdbnames1Restart, frameNum);
 
-    frameNum = 2;
-
-    pdbBase.Init(rsBase, ifBase, pdbnamesBase, frameNum);    
-    pdb1.Init(rs1, if1, pdbnames1, frameNum);
-    pdbN.Init(rsN, ifN, pdbnamesN, frameNum);
-
     config_setup::RestartSettings rsBaseRestart;
     config_setup::RestartSettings rs1Restart;
     config_setup::RestartSettings rsNRestart;
@@ -242,13 +248,15 @@ TEST(ConsistentTrajectoryTest, CheckDCDTrajCoordinates) {
     rsBaseRestart.restartFromCheckpoint = true;
     rs1Restart.restartFromCheckpoint = true;
     rsNRestart.restartFromCheckpoint = true;
+    rsBaseRestart.restartFromBinaryCoorFile = true;
+    rs1Restart.restartFromBinaryCoorFile = true;
+    rsNRestart.restartFromBinaryCoorFile = true;
 
-
-    pdbBaseRestart.Init(rsBaseRestart, ifNull, pdbnamesBaseRestart);    
-    pdb1Restart.Init(rs1Restart, ifNull, pdbnames1Restart);
-    pdbNRestart.Init(rsNRestart, ifNull, pdbnamesNRestart);
+    pdbBaseRestart.Init(rsBaseRestart, ifBaseRestart, pdbnamesBaseRestart);    
+    pdb1Restart.Init(rs1Restart, if1Restart, pdbnames1Restart);
+    pdbNRestart.Init(rsNRestart, ifNRestart, pdbnamesNRestart);
     
-    for (uint i = 0; i < pdbStart.atoms.count; ++i){
+    for (uint i = 0; i < pdbBase.atoms.count; ++i){
         /* Find mol i's chain index in restart output files */
         ptrdiff_t pos = find(pdbBaseRestart.atoms.chainLetter.begin(), 
             pdbBaseRestart.atoms.chainLetter.end(), pdbBase.atoms.chainLetter[i])
@@ -258,7 +266,7 @@ TEST(ConsistentTrajectoryTest, CheckDCDTrajCoordinates) {
         EXPECT_EQ(pdbBase.atoms.x[i] == pdbBaseRestart.atoms.x[pos], true);
     }
 
-    for (uint i = 0; i < pdbStart.atoms.count; ++i){
+    for (uint i = 0; i < pdb1.atoms.count; ++i){
         /* Find mol i's chain index in restart output files */
         ptrdiff_t pos = find(pdb1Restart.atoms.chainLetter.begin(), 
             pdb1Restart.atoms.chainLetter.end(), pdb1.atoms.chainLetter[i])
@@ -268,7 +276,7 @@ TEST(ConsistentTrajectoryTest, CheckDCDTrajCoordinates) {
         EXPECT_EQ(pdb1.atoms.x[i] == pdb1Restart.atoms.x[pos], true);
     }
 
-    for (uint i = 0; i < pdbStart.atoms.count; ++i){
+    for (uint i = 0; i < pdbN.atoms.count; ++i){
         /* Find mol i's chain index in restart output files */
         ptrdiff_t pos = find(pdbNRestart.atoms.chainLetter.begin(), 
             pdbNRestart.atoms.chainLetter.end(), pdbN.atoms.chainLetter[i])
