ab76ae489e04a132113a8e8011704ef5dc14f827
GregorySchwing <gschwing@uno.edu>
2021-09-30 20:36:39 -0500

    metamake with g option

--- metamake.sh
+++ metamake.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+# Check if nvcc is available
 use_cuda=0
 use_profiler=0
+use_gtest=0
+use_gcc=0
+use_mpi=0
+use_debug=0
+MPI="off"
+ENSEMBLES=""
+CMAKEARGS=""
+
 
 # Check if nvcc is available
 if command -v nvcc &> /dev/null
@@ -76,51 +85,100 @@ then
 	fi
 fi
 
-# check to see if we passed the profile flag
-while (( $# > 0 )); do
-	if [[ "$1" != --* ]]; then
-		echo "ERROR: Expected an option beginning with -- but found $1"
-		echo "Available options are: --with-profiling"
-		exit 1
-	fi
-	case "$1" in
-	--with-profiling)
-	shift
-	use_profiler=1
-	;;
-	*)
-		echo "ERROR: unknown option $1"
-		echo "Available options are: --with-profiling"
-		exit 1
-	;;
+while getopts 'mptgd' opt; do
+    case "$opt" in
+        p)
+            use_profiler=1;;
+        m)
+            use_mpi=1
+            CMAKEARGS+="-DGOMC_MPI=on ";;
+        g)
+            use_gcc=1;;
+        t)
+            use_gtest=1;;
+        d)
+            use_debug=1;;
+        *)  echo 'Error in command line options' >&2
+            echo "Available options are: "
+            echo "-p (NVTX tags),"
+            echo "-t (disables Intel compiler to allow GTests to compile),"
+            echo "-m, enables MPI support (Required for Parallel Tempering)"
+            echo "-d, enables Debug Mode compilation"
+            echo "For combined usage: -ptmg"
+            exit 1
     esac
+done
 
+shift "$(( OPTIND - 1 ))"
+
+while [ "$#" -ne 0 ]; do
+    case "$1" in
+        NVT|NPT|GCMC|GEMC|GPU_NVT|GPU_NPT|GPU_GCMC|GPU_GEMC)                   # or just:  -t|--t*)
+            ENSEMBLES+="$1 ";;
+         [!-]*) echo 'Error in Ensembles' >&2
+                echo 'Valid Options: {NVT|NPT|GCMC|GEMC|GPU_NVT|GPU_NPT|GPU_GCMC|GPU_GEMC}' >&2
+                exit 1;;# non-option detected, break out of loop
+        --)    shift; break ;; # explicit end of options detected, break
+        *)  echo 'Error in command line options' >&2
+            exit 1
+    esac
     shift
 done
 
 mkdir -p bin
 cd bin
-ICC_PATH="$(which icc 2> /dev/null)"
-ICPC_PATH="$(which icpc 2> /dev/null)"
-# if intel was not available, link to gcc
-if [ -z "$COMPILER_C_PATH" ]
-then
-  COMPILER_C_PATH="$(which gcc 2> /dev/null)"
-  COMPILER_CXX_PATH="$(which g++ 2> /dev/null)"
+
+if (( !$use_gtest )); then
+    if (( !$use_gcc )); 
+    then
+        ICC_PATH="$(which icc 2> /dev/null)"
+        ICPC_PATH="$(which icpc 2> /dev/null)"
+        if [ -z "$ICC_PATH" ]
+        then
+            export CC="$(which gcc 2> /dev/null)"
+            export CXX="$(which g++ 2> /dev/null)"
+        else
+            export CC=${ICC_PATH}
+            export CXX=${ICPC_PATH}
+        fi
+    else
+        export CC="$(which gcc 2> /dev/null)"
+        export CXX="$(which g++ 2> /dev/null)"
+    fi
+else
+    if (( $use_mpi )); 
+    then
+        ENSEMBLES+="GOMC_NVT_MPI_Test "
+		ENSEMBLES+="GOMC_NPT_MPI_Test "
+		ENSEMBLES+="GOMC_GCMC_MPI_Test "
+		ENSEMBLES+="GOMC_GEMC_MPI_Test "
+        CMAKEARGS+="-DGOMC_GTEST_MPI=on "
+    else
+        ENSEMBLES+="GOMC_NVT_Test "
+        ENSEMBLES+="GOMC_NPT_Test "
+        ENSEMBLES+="GOMC_GCMC_Test "
+        ENSEMBLES+="GOMC_GEMC_Test "
+        CMAKEARGS+="-DGOMC_GTEST=on "
+    fi
+    export CC="$(which gcc 2> /dev/null)"
+    export CXX="$(which g++ 2> /dev/null)"
 fi
-export CC=${ICC_PATH}
-export CXX=${ICPC_PATH}
+
+echo "Ensembles To Compile: $ENSEMBLES"
 
 if (( $use_profiler )); then
     if (( $use_cuda )); then
       	echo "Enabling NVTX profiling for CUDA "
-	  	cmake .. -DGOMC_NVTX_ENABLED=1
+	    CMAKEARGS+="-DGOMC_NVTX_ENABLED=1 "
     else
       	echo "Warning: Cannot enable NVTX profiling without CUDA enabled."
-		cmake ..
     fi
-else
-	cmake ..
 fi
 
-make -j8
+if (( $use_debug )); then
+	echo "Enabling Debug Compilation "
+	CMAKEARGS+="-DCMAKE_BUILD_TYPE=Debug "
+fi
+
+cmake .. $CMAKEARGS
+make -j8 $ENSEMBLES
