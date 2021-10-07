f8c24ac5927ecfe2902060408a4683517fd7e3ee
GregorySchwing <gschwing@uno.edu>
2021-10-07 14:48:22 -0500

    Starting towards making variables belonging to Molecules private so I make sure we access them through checkpoint layer

--- test/src/CheckpointTest.cpp
+++ test/src/CheckpointTest.cpp
@@ -21,18 +21,68 @@ TEST(CheckpointTest, CheckAR_KR) {
     chdir("./test/input/Systems/AR_KR/Base/");
     Simulation base("in.conf");
     base.RunSimulation();
-    /*
+
     chdir("../K_1");
     Simulation K_1("in.conf");
-    K_1.RunSimulation();
+    K_1.RunSimulation();  
     chdir("../K_N");
     Simulation K_N("in.conf");
-    K_N.RunSimulation();
-    chdir("../../../../..");
-    */
+
+    //K_N.RunSimulation();
+    //chdir("../../../../..");
+    
     MoleculeLookup & base_ml = base.GetMolLookup();
     for (int i = 0; i < base_ml.molLookupCount; ++i){
         EXPECT_EQ(base_ml.originalMoleculeIndices[i] == base_ml.molLookup[i], true);
         EXPECT_EQ(base_ml.permutedMoleculeIndices[i] == base_ml.molLookup[i], true);
     }
+    for (int i = 0; i < base_ml.molLookupCount; ++i){
+        EXPECT_EQ(base_ml.originalMoleculeIndices[i] == base_ml.molLookup[i], true);
+        EXPECT_EQ(base_ml.permutedMoleculeIndices[i] == base_ml.molLookup[i], true);
+    }
+        for (int i = 0; i < base_ml.molLookupCount; ++i){
+        EXPECT_EQ(base_ml.originalMoleculeIndices[i] == base_ml.molLookup[i], true);
+        EXPECT_EQ(base_ml.permutedMoleculeIndices[i] == base_ml.molLookup[i], true);
+    }
+    
+    for (int i = 0; i < base_ml.molLookupCount; ++i){
+        std::cout << base_ml.molLookup[i] << " ";
+    }        
+    std::cout << std::endl;
+    for (int i = 0; i < base_ml.molLookupCount; ++i){
+        std::cout << base_ml.permutedMoleculeIndices[i] << " ";
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < base_ml.molLookupCount; ++i){
+        std::cout << base_ml.originalMoleculeIndices[i] << " ";
+    }
+    std::cout << std::endl;
+
+    MoleculeLookup & K_1_ml = K_1.GetMolLookup();
+    for (int i = 0; i < K_1_ml.molLookupCount; ++i){
+        std::cout << K_1_ml.molLookup[i] << " ";
+    }        
+    std::cout << std::endl;
+    for (int i = 0; i < K_1_ml.molLookupCount; ++i){
+        std::cout << K_1_ml.permutedMoleculeIndices[i] << " ";
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < K_1_ml.molLookupCount; ++i){
+        std::cout << K_1_ml.originalMoleculeIndices[i] << " ";
+    }
+    std::cout << std::endl;
+
+    MoleculeLookup & K_N_ml = K_N.GetMolLookup();
+    for (int i = 0; i < K_N_ml.molLookupCount; ++i){
+        std::cout << K_N_ml.molLookup[i] << " ";
+    }        
+    std::cout << std::endl;
+    for (int i = 0; i < K_N_ml.molLookupCount; ++i){
+        std::cout << K_N_ml.permutedMoleculeIndices[i] << " ";
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < K_N_ml.molLookupCount; ++i){
+        std::cout << K_N_ml.originalMoleculeIndices[i] << " ";
+    }
+    std::cout << std::endl;
 }
