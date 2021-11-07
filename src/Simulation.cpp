4f07909c7eabe5f51962e559c2c5437e594140e4
GregorySchwing <gschwing@uno.edu>
2021-11-06 21:47:17 -0500

    A

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -58,8 +58,8 @@ Simulation::~Simulation()
   delete system;
   delete staticValues;
 #ifdef GOMC_CUDA
-  CUDAMemoryManager::isFreed();
-  cudaDeviceReset();
+  //CUDAMemoryManager::isFreed();
+  //cudaDeviceReset();
 #endif
   GOMC_EVENT_STOP(1, GomcProfileEvent::DESTRUCTION);
 }
