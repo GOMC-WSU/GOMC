97a394349a8cabc7bea0e28bd4a27637caffb945
GregorySchwing <gschwing@uno.edu>
2021-11-17 11:44:30 -0600

    RM memchk script

--- cuda-memcheck-script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Run script with the executable as the commandline argument
-cuda-memcheck --tool memcheck --leak-check full --show-backtrace yes --save memcheck-results%p.log $*
-#cuda-memcheck --tool memcheck --leak-check full --show-backtrace yes --save memcheck-results%p.log $*
-#cuda-memcheck --tool racecheck --save memcheck-results%p.log $*
-#cuda-memcheck --save memcheck-results%p.log $*
