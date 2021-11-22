0ae4cc6fff8e7bf46ce04359fd385e5710f29b43
Loren Schwiebert <loren@wayne.edu>
2021-11-21 22:26:11 -0500

    Fix #380 Use constant qqFact from NumLib.h in GPU code

--- lib/NumLib.h
+++ lib/NumLib.h
@@ -24,17 +24,15 @@ along with this program, also can be found at <http://www.gnu.org/licenses/>.
 #ifndef SMALL_WEIGHT
 #define SMALL_WEIGHT 1.0e-38
 #endif
-
-#ifdef GOMC_CUDA
-//Need a separate float constant for device code with the MSVC compiler
-//See CUDA Programming Guide section I.4.13 for details 
-static const __device__ double qqFactGPU = 167103.208067979;
-#endif
-
 namespace num
 {
 static const double dbl_margin = 0.00001;
 static const double qqFact = 167103.208067979;
+#ifdef __CUDA_ARCH__
+//Need a separate float constant for device code with the MSVC compiler
+//See CUDA Programming Guide section I.4.13 for details 
+static const __device__ double qqFactGPU = qqFact;
+#endif
 static const double BIGNUM = DBL_MAX;
 static const uint VDW_STD_KIND = 0, VDW_SHIFT_KIND = 1, VDW_SWITCH_KIND = 2;
 
