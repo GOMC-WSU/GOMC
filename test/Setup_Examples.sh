3fdbdbfda60b6e624ede16fe2f0ef94a2127de4a
GregorySchwing <gschwing@uno.edu>
2021-11-22 13:52:26 -0600

    wrote scripts to automate integration testing

---
+++ test/Setup_Examples.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+git clone https://github.com/GOMC-WSU/GOMC_Examples.git
+mkdir integration
+cd integration
+
+mkdir new_feature_cpu
+cp -frd GOMC_Examples new_feature_cpu
+
+mkdir ref_cpu
+cp -frd GOMC_Examples ref_cpu
+
+mkdir new_feature_gpu
+cp -frd GOMC_Examples new_feature_gpu
+
+mkdir ref_gpu
+cp -frd GOMC_Examples ref_gpu
+
+
+cd ../..
+branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+echo "Building $branch binaries"
+
+./metamake.sh -g NVT
+mkdir -p test/new_feature_binaries
+cp -frdp ./bin/* test/new_feature_binaries
+
+
+echo "$branch"
+if [ $branch == "development" ]; then
+	echo "I am on development; checking out main"
+	git checkout main
+else
+	echo "I am on $branch; checking out development"
+	git checkout development
+fi
+
+rm -frd bin
+branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+echo "Building $branch binaries"
+./metamake.sh -g NVT
+mkdir -p test/ref_binaries
+cp -frd ./bin/* test/ref_binaries
+cd test
+
