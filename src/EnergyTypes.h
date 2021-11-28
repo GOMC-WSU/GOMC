388d819233f103b3c72fabb65c1c63341caa6277
Loren Schwiebert <loren@wayne.edu>
2021-11-28 17:12:35 -0500

    Restore original formatting of cout after printing energies

--- src/EnergyTypes.h
+++ src/EnergyTypes.h
@@ -516,7 +516,7 @@ inline std::ostream& operator<<(std::ostream& out, Energy& en)
 
   //Restore ostream settings to prior value
   out << std::setprecision(ss);
-  out.setf(ff);
+  out.flags(ff);
   }
   return out;
 }
