c23f91a2f9af3bd0ff92f7e872cb56b2e29e99f5
GregorySchwing <gschwing@uno.edu>
2022-04-12 09:48:34 -0500

    update license

--- src/moves/MoleculeExchange3.h
+++ src/moves/MoleculeExchange3.h
@@ -1,8 +1,8 @@
 /*******************************************************************************
-GPU OPTIMIZED MONTE CARLO (GOMC) 2.70
-Copyright (C) 2018  GOMC Group
-A copy of the GNU General Public License can be found in the COPYRIGHT.txt
-along with this program, also can be found at <http://www.gnu.org/licenses/>.
+GPU OPTIMIZED MONTE CARLO (GOMC) 2.75
+Copyright (C) 2022 GOMC Group
+A copy of the MIT License can be found in the COPYRIGHT.txt
+along with this program, also can be found at <https://opensource.org/licenses/MIT>.
 ********************************************************************************/
 #ifndef MOLECULEEXCHANGE3_H
 #define MOLECULEEXCHANGE3_H
