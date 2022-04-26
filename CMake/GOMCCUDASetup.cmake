19238ac9b93e62114ba373cd84ebbefe714dc324
greg <go2432@wayne.edu>
2022-04-26 14:56:19 -0500

    gencode 80 sm 80

--- CMake/GOMCCUDASetup.cmake
+++ CMake/GOMCCUDASetup.cmake
@@ -1,11 +1,6 @@
 # Find CUDA is enabled, set it up
 set(CUDA_ARCH
-    -gencode=arch=compute_60,code=sm_60
-    -gencode=arch=compute_61,code=sm_61
-    -gencode=arch=compute_70,code=sm_70
-    -gencode=arch=compute_75,code=sm_75
     -gencode=arch=compute_80,code=sm_80 # If you don't need this, comment it or remove it.
-    -gencode=arch=compute_86,code=sm_86 # If you don't need this, comment it or remove it.
 )
 
 
