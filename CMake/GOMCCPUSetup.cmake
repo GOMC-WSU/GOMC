7885d5a3988c107616cae0d5ad5ee28025a9934a
GregorySchwing <gschwing@uno.edu>
2022-04-30 13:14:14 -0500

    compile with avx

--- CMake/GOMCCPUSetup.cmake
+++ CMake/GOMCCPUSetup.cmake
@@ -1,19 +1,19 @@
 #EnsemblePreprocessor defines NVT = 1, GEMC = 2, GCMC = 3, NPT = 4
 #NPT (Isothermal-Isobaric) Ensemble
 
-set(NPT_flags "-DENSEMBLE=4")
+set(NPT_flags "-DENSEMBLE=4 -Ofast -mavx -axAVX,CORE-AVX2")
 set(NPT_name "GOMC_CPU_NPT")
 
 #Grand Canonical Monte Carlo
-set(GC_flags "-DENSEMBLE=3")
+set(GC_flags "-DENSEMBLE=3 -Ofast -mavx -axAVX,CORE-AVX2")
 set(GC_name "GOMC_CPU_GCMC")
 
 #Gibbs Ensemble Monte Carlo
-set(GE_flags "-DENSEMBLE=2")
+set(GE_flags "-DENSEMBLE=2 -Ofast -mavx -axAVX,CORE-AVX2")
 set(GE_name "GOMC_CPU_GEMC")
 
 #NVT (Canonical) Ensemble
-set(NVT_flags "-DENSEMBLE=1")
+set(NVT_flags "-DENSEMBLE=1 -Ofast -mavx -axAVX,CORE-AVX2")
 set(NVT_name "GOMC_CPU_NVT")
 
 set(CMAKE_CXX_STANDARD 14)
