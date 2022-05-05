cf0b415e9827a760f0fce4460dda6062e0d42ed2
GregorySchwing <gschwing@uno.edu>
2022-05-05 16:29:06 -0500

    dont req lt

--- src/GPU/CalculateEnergyCUDAKernel.cu
+++ src/GPU/CalculateEnergyCUDAKernel.cu
@@ -565,7 +565,7 @@ __global__ void MolInterGPU(int gpu_moleculeStart,
     int currentParticle = gpu_moleculeStart + currentParticleIndex;
     int neighborParticle = gpu_cellVector[gpu_cellStartIndex[neighborCell] + neighborParticleIndex];
 
-    if(currentParticle < neighborParticle && gpu_particleMol[currentParticle] != gpu_particleMol[neighborParticle]) {
+    if(gpu_particleMol[currentParticle] != gpu_particleMol[neighborParticle]) {
       // Check if they are within rcut
       double distSq = 0.0;
 
