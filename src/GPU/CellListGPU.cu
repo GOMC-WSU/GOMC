0923e89cb12ecaf5f3fb7515c2530f4bb062d722
greg <go2432@wayne.edu>
2022-05-10 19:07:36 -0500

    update

--- src/GPU/CellListGPU.cu
+++ src/GPU/CellListGPU.cu
@@ -2,12 +2,12 @@
 #include "CellListGPU.cuh"
 
 CellListGPU::CellListGPU(VariablesCUDA * cv):
-gpu_cellVector(cv->gpu_cellVector),
-gpu_mapParticleToCell(cv->gpu_mapParticleToCell),
-gpu_cellStartIndex(cv->gpu_cellStartIndex),
-gpu_neighborList(cv->gpu_neighborList),
-gpu_numberOfCells(cv->gpu_numberOfCells),
-gpu_startOfBoxCellList(cv->gpu_startOfBoxCellList)
+gpu_cellVector(cv->gpu_cellVectorGPURes),
+gpu_mapParticleToCell(cv->gpu_mapParticleToCellGPURes),
+gpu_cellStartIndex(cv->gpu_cellStartIndexGPURes),
+gpu_neighborList(cv->gpu_neighborListGPURes),
+gpu_numberOfCells(cv->gpu_numberOfCellsGPURes),
+gpu_startOfBoxCellList(cv->gpu_startOfBoxCellListGPURes)
 {
 }
 #endif
