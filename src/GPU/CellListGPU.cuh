53208907c669009fea21d77435e033fad7ca5c75
greg <go2432@wayne.edu>
2022-05-10 19:58:41 -0500

    a

--- src/GPU/CellListGPU.cuh
+++ src/GPU/CellListGPU.cuh
@@ -3,19 +3,19 @@
 #ifdef GOMC_CUDA
 
 #include "VariablesCUDA.cuh"
+#include "XYZArray.h"
 
 class CellListGPU {
   public:
     CellListGPU(VariablesCUDA * cv);
-    void GridAll(VariablesCUDA * cv);
+    void MapParticlesToCell(int atomNumber,
+                            XYZArray const &coords);
     // new pair interaction calculation done on GPU
     int *gpu_cellVector, *gpu_mapParticleToCell, *gpu_cellStartIndex;
     // Fixed as long as volume doesnt change
     // Regenerate after volume moves.
     int *gpu_neighborList, *gpu_numberOfCells, *gpu_startOfBoxCellList;
-  private:
-    void MapParticlesToCell(int atomNumber,
-                            int* gpu_mapParticleToCell);
+
 };
 
 #endif
