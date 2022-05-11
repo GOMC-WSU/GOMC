ecad564192a274fb1c300ca7d6d36be20e02d869
greg <go2432@wayne.edu>
2022-05-10 19:56:28 -0500

    krnel

--- src/GPU/CellListGPU.cu
+++ src/GPU/CellListGPU.cu
@@ -11,8 +11,18 @@ gpu_startOfBoxCellList(cv->gpu_startOfBoxCellListGPURes)
 {
 }
 
-void CellListGPU::GridAll(VariablesCUDA * cv){
-
+void CellListGPU::MapParticlesToCell(VariablesCUDA * cv,
+                                    XYZArray const &coords){
+    int atomNumber = coords.Count();
+    cudaMemcpy(vars->gpu_x, currentCoords.x, atomNumber * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(vars->gpu_y, currentCoords.y, atomNumber * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(vars->gpu_z, currentCoords.z, atomNumber * sizeof(double), cudaMemcpyHostToDevice);
 }
+
+__global__ void MapParticlesToCell(int atomNumber,
+                            double* gpu_x,
+                            double* gpu_y,
+                            double* gpu_z,                                
+                            int* gpu_mapParticleToCell){}
 
 #endif
