5cde9944e881a52b0d76948efc9e15fdefd2327b
greg <go2432@wayne.edu>
2022-05-12 16:02:12 -0500

    t

--- src/GPU/ConstantDefinitionsCUDAKernel.cu
+++ src/GPU/ConstantDefinitionsCUDAKernel.cu
@@ -163,10 +163,10 @@ void InitGPUCellList(VariablesCUDA *vars,
   CUMALLOC((void**) &vars->gpu_IterationsReq, 1 * sizeof(int));
 
 
-  vars->(*d_temp_storage_sort) = NULL;
+  *(vars->d_temp_storage_sort) = NULL;
   vars->temp_storage_bytes_sort = &vars->zero1;
 
-  vars->(*d_temp_storage_prefix_sum) = NULL; 
+  *(vars->d_temp_storage_prefix_sum) = NULL; 
   vars->temp_storage_bytes_prefix_sum = &vars->zero2;
 
   cudaMemcpy(vars->gpu_numberOfCells, &numberOfCells[0], numberOfCells.size() * sizeof(int),
