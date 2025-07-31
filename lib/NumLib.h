ebbaee33d5b4a96521a5df1d054baf7b9862e4b1
Loren Schwiebert <loren@wayne.edu>
2025-07-31 11:45:35 -0400

    Restore accidentally deleted files

---
+++ lib/NumLib.h
@@ -0,0 +1,323 @@
+/*******************************************************************************
+GPU OPTIMIZED MONTE CARLO (GOMC) 2.75
+Copyright (C) 2022 GOMC Group
+A copy of the MIT License can be found in License.txt
+along with this program, also can be found at
+<https://opensource.org/licenses/MIT>.
+********************************************************************************/
+#ifndef NUMERIC_LIB_H
+#define NUMERIC_LIB_H
+
+#include "BasicTypes.h" //For uint, XYZ
+#include <iostream>
+#include <limits> //for double limits
+#include <vector> //for vector average
+
+#ifndef DBL_MAX
+#define DBL_MAX 1.7976931348623158e+308
+#endif
+
+#ifndef SMALL_WEIGHT
+#define SMALL_WEIGHT 1.0e-38
+#endif
+namespace num {
+static const double qqFact = 167103.208067979;
+static const double MIN_EXP_NONZERO_VAL = -708.4;
+static const double BIGNUM = DBL_MAX;
+static const uint VDW_STD_KIND = 0, VDW_SHIFT_KIND = 1, VDW_SWITCH_KIND = 2;
+
+inline bool approximatelyEqual(double a, double b, double epsilon) {
+  if (abs(a) < 1.0 || abs(b) < 1.0) {
+    return abs(a - b) <= epsilon;
+  } else {
+    return std::abs(a - b) <=
+           ((std::abs(a) < std::abs(b) ? std::abs(b) : std::abs(a)) * epsilon);
+  }
+}
+
+template <typename T> inline void BoundGt(double &val, const double bound) {
+  if (val > bound)
+    val = bound;
+}
+
+template <typename T> inline void BoundLt(double &val, const double bound) {
+  if (val < bound)
+    val = bound;
+}
+
+template <typename T> inline void BoundNZDecimal(T &val, const int mult) {
+  BoundLt<T>(val, std::numeric_limits<T>::min() * mult);
+}
+
+template <typename T>
+inline void Bound(T &val, const double lower, const double upper) {
+  BoundLt<T>(val, lower);
+  BoundGt<T>(val, upper);
+}
+
+// Arithmetic mean.
+inline double MeanA(const uint v1, const uint v2) { return (v1 + v2) / 2.0; }
+// Geometric mean.
+inline double MeanG(const double v1, const double v2) { return sqrt(v1 * v2); }
+// Arithmetic mean.
+inline double MeanA(std::vector<double> const &v1,
+                    std::vector<double> const &v2, const uint ix1,
+                    const uint ix2) {
+  return (v1[ix1] + v2[ix2]) * 0.5;
+}
+// Arithmetic mean.
+inline double MeanA(std::vector<uint> const &v1, std::vector<uint> const &v2,
+                    const uint ix1, const uint ix2) {
+  return ((double)(v1[ix1] + v2[ix2])) * 0.5;
+}
+// Geometric mean.
+inline double MeanG(std::vector<double> const &v1,
+                    std::vector<double> const &v2, const uint ix1,
+                    const uint ix2) {
+  return sqrt(v1[ix1] * v2[ix2]);
+}
+
+inline double MeanG(std::vector<uint> const &v1, std::vector<uint> const &v2,
+                    const uint ix1, const uint ix2) {
+  return sqrt(v1[ix1] * v2[ix2]);
+}
+
+// return n!
+inline double Factorial(const uint n) {
+  double result = 1.0;
+  for (uint i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+// return (n+count)!/n!
+inline double Factorial(const uint n, const uint count) {
+  double result = 1.0;
+  for (uint i = 1; i <= count; i++) {
+    result *= n + i;
+  }
+  return result;
+}
+
+template <class Type> inline Type Sq(const Type v) { return v * v; }
+template <class Type> inline Type Cb(const Type v) { return v * v * v; }
+template <class Type> inline void Cb(Type &s, Type &c, const Type v) {
+  s = v * v;
+  c = s * v;
+}
+
+inline double POW(const double d2, const double d4, const double d6, uint e) {
+  double result = (e & 0x1 ? sqrt(d2) : 1.0);
+  e >>= 1;
+  switch (e) {
+  case 0:
+    break;
+  case 1:
+    result *= d2;
+    break;
+  case 2:
+    result *= d4;
+    break;
+  case 3:
+    result *= d6;
+    break;
+  case 4:
+    result *= Sq(d4);
+    break;
+  case 5:
+    result *= d4 * d6;
+    break;
+  case 6:
+    result *= Sq(d6);
+    break;
+  case 7:
+    result *= Sq(d6) * d2;
+    break;
+  case 8:
+    result *= Sq(d6) * d4;
+    break;
+  case 9:
+    result *= Sq(d6) * d6;
+    break;
+  case 10:
+    result *= Sq(d6) * Sq(d4);
+    break;
+  case 11:
+    result *= Sq(Sq(d4)) * d6;
+    break;
+  case 12:
+    result *= Sq(Sq(d6));
+    break;
+  case 13:
+    result *= Sq(Sq(d6)) * d2;
+    break;
+  case 14:
+    result *= Sq(Sq(d6)) * d4;
+    break;
+  case 15:
+    result *= Sq(Sq(d6)) * d6;
+    break;
+  case 16:
+    result *= Sq(Sq(Sq(d4)));
+    break;
+  case 17:
+    result *= Sq(Sq(Sq(d4))) * d2;
+    break;
+  case 18:
+    result *= Sq(Sq(d6) * d6);
+    break;
+  case 19:
+    result *= Sq(Sq(d6) * d6) * d2;
+    break;
+  case 20:
+    result *= Sq(Sq(d6 * d4));
+    break;
+  case 21:
+    result *= Sq(Sq(d6 * d4)) * d2;
+    break;
+  case 22:
+    result *= Sq(Sq(Sq(d4)) * d6);
+    break;
+  case 23:
+    result *= Sq(Sq(Sq(d4)) * d6) * d2;
+    break;
+  case 24:
+    result *= Sq(Sq(Sq(d6)));
+    break;
+  case 25:
+    result *= Sq(Sq(Sq(d6))) * d2;
+    break;
+  }
+  return result;
+}
+
+// Class to define the function used in Zbrent
+class Exp6Fun {
+public:
+  Exp6Fun(const float a, const float s, const float r = 0.0) : sigma(s),
+      alpha(a), rmin(r) {}
+  virtual ~Exp6Fun(){};
+  virtual float operator()(float x) = 0;
+
+  float sigma, alpha, rmin;
+};
+
+class RminFun : public Exp6Fun {
+public:
+  RminFun(double a, double s) : Exp6Fun(a, s) {}
+  virtual ~RminFun(){};
+  virtual float operator()(float x) {
+    double rep = (6.0 / alpha) * exp(alpha * (1.0 - sigma / x));
+    double at = pow(x / sigma, 6.0);
+    return (float)(rep - at);
+  }
+};
+
+class RmaxFun : public Exp6Fun {
+public:
+  RmaxFun(double a, double s, double r) : Exp6Fun(a, s, r) {}
+  virtual ~RmaxFun(){};
+  virtual float operator()(float x) {
+    double rep = (-1.0 / rmin) * exp(alpha * (1.0 - x / rmin));
+    double at = pow(rmin / x, 6.0) / x;
+    return (float)(rep + at);
+  }
+};
+
+// Using Brent’s method, find the root of a function func known to lie between
+// x1 and x2. The root, returned as zbrent, will be refined until its accuracy is
+// tol. Brent, R.P. 1973, Algorithms for Minimization without Derivatives
+// (Englewood Cliffs, NJ: Prentice-Hall) Forsythe, G.E., Malcolm, M.A., and
+// Moler, C.B. 1977, Computer Methods for Mathematical Computations (Englewood
+// Cliffs, NJ: Prentice-Hall)
+inline double Zbrent(Exp6Fun *func, float x1, float x2, float tol) {
+  const int ITMAX = 100;    // Maximum allowed number of iterations.
+  const float EPS = 3.0e-8; // Machine floating-point precision.
+  int iter;
+  float a = x1, b = x2, c = x2;
+  float d, e, min1, min2;
+  float fa = (*func)(a), fb = (*func)(b);
+  float fc, p, q, r, s, tol1, xm;
+
+  if ((fa > 0.0 && fb > 0.0) || (fa < 0.0 && fb < 0.0)) {
+    std::cout << "Root must be bracketed in zbrent!\n";
+    exit(EXIT_FAILURE);
+  }
+
+  fc = fb;
+
+  for (iter = 1; iter <= ITMAX; iter++) {
+    if ((fb > 0.0 && fc > 0.0) || (fb < 0.0 && fc < 0.0)) {
+      c = a; // Rename a, b, c and adjust bounding interval
+      fc = fa;
+      e = d = b - a;
+    }
+
+    if (fabs(fc) < fabs(fb)) {
+      a = b;
+      b = c;
+      c = a;
+      fa = fb;
+      fb = fc;
+      fc = fa;
+    }
+
+    tol1 = 2.0 * EPS * fabs(b) + 0.5 * tol; // Convergence check.
+    xm = 0.5 * (c - b);
+    if (fabs(xm) <= tol1 || fb == 0.0) {
+      return (double)b;
+    }
+
+    if (fabs(e) >= tol1 && fabs(fa) > fabs(fb)) {
+      s = fb / fa; // Attempt inverse quadratic interpolation.
+      if (a == c) {
+        p = 2.0 * xm * s;
+        q = 1.0 - s;
+      } else {
+        q = fa / fc;
+        r = fb / fc;
+        p = s * (2.0 * xm * q * (q - r) - (b - a) * (r - 1.0));
+        q = (q - 1.0) * (r - 1.0) * (s - 1.0);
+      }
+
+      if (p > 0.0) {
+        q = -q; // Check whether in bounds.
+      }
+
+      p = fabs(p);
+      min1 = 3.0 * xm * q - fabs(tol1 * q);
+      min2 = fabs(e * q);
+      if (2.0 * p < (min1 < min2 ? min1 : min2)) {
+        e = d; // Accept interpolation.
+        d = p / q;
+      } else {
+        d = xm; // Interpolation failed, use bisection.
+        e = d;
+      }
+    } else {
+      // Bounds decreasing too slowly, use bisection.
+      d = xm;
+      e = d;
+    }
+
+    a = b; // Move last best guess to a.
+    fa = fb;
+    if (fabs(d) > tol1) { // Evaluate new trial root.
+      b += d;
+    } else {
+      // b += SIGN(tol1, xm);
+      b += ((xm) >= 0.0 ? fabs(tol1) : -fabs(tol1));
+    }
+
+    fb = (*func)(b);
+  }
+  std::cout << "Maximum number of iterations exceeded in zbrent!\n";
+  exit(EXIT_FAILURE);
+
+  return 0.0; // Never get here.
+}
+
+} // namespace num
+
+#endif /*NUMERIC_LIB_H*/
---
+++ src/cbmc/DCRotateCOM.cpp
@@ -0,0 +1,355 @@
+/*******************************************************************************
+GPU OPTIMIZED MONTE CARLO (GOMC) 2.75
+Copyright (C) 2022 GOMC Group
+A copy of the MIT License can be found in License.txt
+along with this program, also can be found at
+<https://opensource.org/licenses/MIT>.
+********************************************************************************/
+#include "DCRotateCOM.h"
+#include "DCData.h"
+#include "Forcefield.h"
+#include "MolSetup.h"
+#include "NumLib.h"
+#include "PRNG.h"
+#include "TrialMol.h"
+/*
+This file is only called from MoleculeExchange and IntraMoleculeExchange file.
+This file depend on ensemble kind, swap the COM of small molecules with one
+large molecule.
+
+Molecule is allowed to have trial position in Box or trial position
+in cavity (HasCav()), or no trial position (COMFix()).
+
+Molecule is allowed to have rotation around COM or around Z-axis
+*/
+
+namespace cbmc {
+
+DCRotateCOM::DCRotateCOM(DCData *data, const mol_setup::MolKind kind)
+    : data(data), rotateMatrix(3), invMatrix(3) {
+  rotateMatrix.Set(0, 0.0, 0.0, 0.0);
+  rotateMatrix.Set(1, 0.0, 0.0, 0.0);
+  rotateMatrix.Set(2, 0.0, 0.0, 1.0);
+  atomNumber = kind.atoms.size();
+  multiPosRotions = new XYZArray[atomNumber];
+
+  if (data->totalTrials < 1) {
+    std::cout << "Error: CBMC first or secondary atom trials must be greater "
+                 "than 0.\n";
+    exit(EXIT_FAILURE);
+  }
+  for (uint i = 0; i < atomNumber; ++i) {
+    multiPosRotions[i] = XYZArray(data->totalTrials);
+  }
+}
+
+void DCRotateCOM::RandRotateZ() {
+  PRNG &prng = data->prng;
+  double theta = prng();
+  theta *= 2.0 * M_PI;
+  theta -= M_PI;
+  double cosTheta = cos(theta);
+  double sinTheta = sin(theta);
+  rotateMatrix.Set(0, cosTheta, -sinTheta, 0.0);
+  rotateMatrix.Set(1, sinTheta, cosTheta, 0.0);
+}
+
+void DCRotateCOM::PrepareNew(TrialMol &newMol, uint molIndex) {
+  newMol.SetWeight(1.0);
+  // old center of mass
+  oldCOM = newMol.GetCOM();
+}
+
+void DCRotateCOM::PickTransferCOMNew(TrialMol &newMol, uint molIndex) {
+  PRNG &prng = data->prng;
+
+  if (newMol.COMFix()) {
+    COM = newMol.GetCavityCenter();
+  } else if (newMol.HasCav()) {
+    // new center of mass that need to be transfered
+    // Pick molecule in cav dimension
+    prng.FillWithRandomInCavity(COM, newMol.GetCavity());
+    // rotate using cavity matrix
+    COM = newMol.Transform(COM);
+    // add center
+    COM += newMol.GetCavityCenter();
+  } else {
+    prng.FillWithRandom(COM, data->axes, newMol.GetBox());
+  }
+
+  XYZ diff = COM - oldCOM;
+
+  for (uint p = 0; p < atomNumber; p++) {
+    newMol.SetAtomCoords(p, newMol.AtomPosition(p) + diff);
+  }
+
+  oldCOM = COM;
+}
+
+void DCRotateCOM::PrepareOld(TrialMol &oldMol, uint molIndex) {
+  oldMol.SetWeight(1.0);
+  // old center of mass
+  oldCOM = oldMol.GetCOM();
+  COM = oldCOM;
+}
+
+void DCRotateCOM::PickTransferCOMOld(TrialMol &oldMol, uint molIndex) {
+  PRNG &prng = data->prng;
+  // new center of mass that need to be transfered
+  if (oldMol.HasCav()) {
+    // Pick molecule in cav dimension
+    prng.FillWithRandomInCavity(COM, oldMol.GetCavity());
+    // rotate using cavity matrix
+    COM = oldMol.Transform(COM);
+    // add center
+    COM += oldMol.GetCavityCenter();
+  } else
+    prng.FillWithRandom(COM, data->axes, oldMol.GetBox());
+
+  XYZ diff = COM - oldCOM;
+
+  for (uint p = 0; p < atomNumber; p++) {
+    oldMol.SetAtomCoords(p, oldMol.AtomPosition(p) + diff);
+  }
+
+  oldCOM = COM;
+}
+
+void DCRotateCOM::BuildNew(TrialMol &newMol, uint molIndex) {
+  PRNG &prng = data->prng;
+  const CalculateEnergy &calc = data->calc;
+  // const Ewald *calcEwald = data->calcEwald;
+  const Forcefield &ff = data->ff;
+  uint nLJTrials = data->nLJTrialsNth;
+  uint fLJTrials = data->nLJTrialsFirst;
+  uint totalTrials = data->totalTrials;
+  double *ljWeights = data->ljWeightsT;
+  double *inter = data->interT;
+  double *real = data->realT;
+  bool *overlap = data->overlapT;
+  RotationMatrix spin;
+
+  std::fill_n(inter, totalTrials, 0.0);
+  std::fill_n(real, totalTrials, 0.0);
+  std::fill_n(ljWeights, totalTrials, 0.0);
+  std::fill_n(overlap, totalTrials, false);
+
+  if (atomNumber == 1) {
+    nLJTrials = 1;
+    totalTrials = fLJTrials;
+  }
+
+  if (newMol.COMFix()) {
+    fLJTrials = 1;
+    totalTrials = nLJTrials;
+    // if we rotate around backbone we need to calc the rotation matrix
+    if (newMol.RotateBB()) {
+      // find the inverse matrix of molecule that we insert
+      XYZ backBone;
+      if (atomNumber != 1) {
+        backBone = newMol.GetCoords().Difference(newMol.GetAtomBB(0),
+                                                 newMol.GetAtomBB(1));
+      } else {
+        backBone = prng.RandomUnitVect();
+      }
+      XYZArray T(3);
+      geom::SetBasis(T, backBone);
+      geom::TransposeMatrix(invMatrix, T);
+    }
+  }
+
+  for (uint p = 0; p < fLJTrials; ++p) {
+    // Pick a new position for COM and transfer the molecule
+    PickTransferCOMNew(newMol, molIndex);
+    // get info about existing geometry
+    newMol.ShiftBasis(COM);
+    const XYZ center = COM;
+    uint index = p * nLJTrials;
+
+    for (uint a = 0; a < atomNumber; ++a) {
+      multiPosRotions[a].Set(index, newMol.AtomPosition(a));
+      multiPosRotions[a].Add(index, -center);
+    }
+
+    // Rotational trial the molecule around COM
+    for (uint r = nLJTrials; r-- > 0;) {
+      if (newMol.RotateBB()) {
+        // we only perform rotation around z axis
+        RandRotateZ();
+      } else {
+        // convert chosen torsion to 3D positions
+        double u1, u2, u3;
+        u1 = prng();
+        u2 = prng();
+        u3 = prng();
+        spin = RotationMatrix::UniformRandom(u1, u2, u3);
+      }
+
+      for (uint a = 0; a < atomNumber; ++a) {
+        if (newMol.RotateBB()) {
+          XYZ coord = multiPosRotions[a][index];
+          // transform backbone to z axis
+          coord = geom::Transform(invMatrix, coord);
+          // rotate around z
+          coord = geom::Transform(rotateMatrix, coord);
+          // transfer backbone to cavity orientation
+          coord = newMol.Transform(coord);
+          multiPosRotions[a].Set(index + r, coord);
+        } else {
+          // find positions
+          multiPosRotions[a].Set(index + r,
+                                 spin.Apply(multiPosRotions[a][index]));
+        }
+        multiPosRotions[a].Add(index + r, center);
+      }
+    }
+  }
+
+  for (uint a = 0; a < atomNumber; ++a) {
+    data->axes.WrapPBC(multiPosRotions[a], newMol.GetBox());
+    calc.ParticleInter(inter, real, multiPosRotions[a], overlap, a, molIndex,
+                       newMol.GetBox(), totalTrials);
+  }
+
+  double stepWeight = 0.0;
+  for (uint lj = 0; lj < totalTrials; ++lj) {
+    // Skip exp() calculation for small values for efficiency and to avoid
+    // subnormal values that contribute to differences between processors.
+    // Value chosen mathematically: See cppreference.com exp function notes.
+    // Note: ljWeights prefilled with 0.0, so don't need to initialize it.
+    double betaWeight = -ff.beta * (inter[lj] + real[lj]);
+    if (betaWeight >= num::MIN_EXP_NONZERO_VAL) {
+      ljWeights[lj] = std::exp(betaWeight);
+      stepWeight += ljWeights[lj];
+    }
+  }
+  uint winner = prng.PickWeighted(ljWeights, totalTrials, stepWeight);
+  for (uint a = 0; a < atomNumber; ++a) {
+    newMol.AddAtom(a, multiPosRotions[a][winner]);
+  }
+
+  newMol.UpdateOverlap(overlap[winner]);
+  newMol.AddEnergy(
+      Energy(0.0, 0.0, inter[winner], real[winner], 0.0, 0.0, 0.0));
+  newMol.MultWeight(stepWeight / totalTrials);
+}
+
+void DCRotateCOM::BuildOld(TrialMol &oldMol, uint molIndex) {
+  PRNG &prng = data->prng;
+  const CalculateEnergy &calc = data->calc;
+  // const Ewald * calcEwald = data->calcEwald;
+  const Forcefield &ff = data->ff;
+  uint nLJTrials = data->nLJTrialsNth;
+  uint fLJTrials = data->nLJTrialsFirst;
+  uint totalTrials = data->totalTrials;
+  double *ljWeights = data->ljWeightsT;
+  double *inter = data->interT;
+  double *real = data->realT;
+  bool *overlap = data->overlapT;
+  RotationMatrix spin;
+
+  std::fill_n(inter, totalTrials, 0.0);
+  std::fill_n(real, totalTrials, 0.0);
+  std::fill_n(ljWeights, totalTrials, 0.0);
+  std::fill_n(overlap, totalTrials, false);
+
+  if (atomNumber == 1) {
+    nLJTrials = 1;
+    totalTrials = fLJTrials;
+  }
+
+  if (oldMol.COMFix()) {
+    fLJTrials = 1;
+    totalTrials = nLJTrials;
+    // if we rotate around backbone of the molecule
+    if (oldMol.RotateBB()) {
+      // find the inverse matrix of cavity
+      oldMol.TransposeMatrix(invMatrix);
+    }
+  }
+
+  const XYZ orgCenter = COM;
+
+  for (uint p = 0; p < fLJTrials; ++p) {
+    // First trial is current configuration
+    // get info about existing geometry
+    oldMol.ShiftBasis(COM);
+    const XYZ center = COM;
+    uint index = p * nLJTrials;
+
+    for (uint a = 0; a < atomNumber; ++a) {
+      // get position and shift to origin
+      multiPosRotions[a].Set(index, oldMol.AtomPosition(a));
+      multiPosRotions[a].Add(index, -center);
+    }
+
+    // Rotational trial the molecule around COM
+    for (uint r = nLJTrials; r-- > 0;) {
+      if ((index + r) == 0)
+        continue;
+
+      if (oldMol.RotateBB()) {
+        // we only perform rotation around z axis
+        RandRotateZ();
+      } else {
+        // convert chosen torsion to 3D positions
+        double u1, u2, u3;
+        u1 = prng();
+        u2 = prng();
+        u3 = prng();
+        spin = RotationMatrix::UniformRandom(u1, u2, u3);
+      }
+
+      for (uint a = 0; a < atomNumber; ++a) {
+        if (oldMol.RotateBB()) {
+          XYZ coord = multiPosRotions[a][index];
+          // transform backbone to z axis
+          coord = geom::Transform(invMatrix, coord);
+          // rotate around z
+          coord = geom::Transform(rotateMatrix, coord);
+          // transfer backbone to cavity orientation
+          coord = oldMol.Transform(coord);
+          multiPosRotions[a].Set(index + r, coord);
+        } else {
+          // find positions
+          multiPosRotions[a].Set(index + r,
+                                 spin.Apply(multiPosRotions[a][index]));
+        }
+        multiPosRotions[a].Add(index + r, center);
+      }
+    }
+
+    // Pick a new position for COM and transfer the molecule
+    PickTransferCOMOld(oldMol, molIndex);
+  }
+
+  for (uint a = 0; a < atomNumber; ++a) {
+    multiPosRotions[a].Add(0, orgCenter);
+    data->axes.WrapPBC(multiPosRotions[a], oldMol.GetBox());
+    calc.ParticleInter(inter, real, multiPosRotions[a], overlap, a, molIndex,
+                       oldMol.GetBox(), totalTrials);
+  }
+
+  double stepWeight = 0.0;
+  for (uint lj = 0; lj < totalTrials; ++lj) {
+    // Skip exp() calculation for small values for efficiency and to avoid
+    // subnormal values that contribute to differences between processors.
+    // Value chosen mathematically: See cppreference.com exp function notes.
+    // Note: ljWeights prefilled with 0.0, so don't need to initialize it.
+    double betaWeight = -ff.beta * (inter[lj] + real[lj]);
+    if (betaWeight >= num::MIN_EXP_NONZERO_VAL) {
+      ljWeights[lj] = std::exp(betaWeight);
+      stepWeight += ljWeights[lj];
+    }
+  }
+
+  for (uint a = 0; a < atomNumber; ++a) {
+    oldMol.AddAtom(a, multiPosRotions[a][0]);
+  }
+
+  oldMol.UpdateOverlap(overlap[0]);
+  oldMol.AddEnergy(Energy(0.0, 0.0, inter[0], real[0], 0.0, 0.0, 0.0));
+  oldMol.MultWeight(stepWeight / totalTrials);
+}
+
+} // namespace cbmc
