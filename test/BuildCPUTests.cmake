6501787d27e98426d24ac10da6d51ce8766c1476
greg <go2432@wayne.edu>
2025-10-20 13:08:13 -0400

    fix how tests are built

--- test/BuildCPUTests.cmake
+++ test/BuildCPUTests.cmake
@@ -1,61 +1,64 @@
-#EnsemblePreprocessor defines NVT = 1, GEMC = 2, GCMC = 3, NPT = 4
+# EnsemblePreprocessor defines:
+# NVT = 1, GEMC = 2, GCMC = 3, NPT = 4
 
+include(GoogleTest)  # Recommended, even if not using gtest_discover_tests()
+
+# Helper function to reduce duplication
+function(add_ensemble_test name ENSEMBLE_ID ENSEMBLE_LABEL)
+    set(flags "-DENSEMBLE=${ENSEMBLE_ID}")
+
+    # My tests are all called name_test.cpp
+    add_executable(${name} 
+        ${GOMCHeaders} ${GOMCSources}
+        ${libHeaders} ${libSources}
+        ${TestHeaders} ${TestSources}
+    )
+
+    target_link_libraries(${name} gtest_main)
+    set_target_properties(${name} PROPERTIES COMPILE_FLAGS "${flags}")
+
+    #
+    # Register each suite as a separate CTest test
+    # Each gtest_filter pattern limits execution to one test suite
+    #
+    add_test(NAME BasicTypesTest_${ENSEMBLE_LABEL}
+             COMMAND ${name} --gtest_filter=BasicTypesTest.*)
+
+    # add_test(NAME CircuitTester_${ENSEMBLE_LABEL}
+    #          COMMAND ${name} --gtest_filter=DialaTest.*)
+
+    add_test(NAME MolLookupTest_${ENSEMBLE_LABEL}
+             COMMAND ${name} --gtest_filter=CheckConsensusBeta.*)
+
+    # add_test(NAME PSFParserTest_${ENSEMBLE_LABEL}
+    #          COMMAND ${name} --gtest_filter=CheckProtAndWaterTest.*)
+
+    if(${ENSEMBLE_ID} EQUAL 2 OR ${ENSEMBLE_ID} EQUAL 3)  # GEMC or GCMC
+        add_test(NAME ConsistentTrajectoryTest_${ENSEMBLE_LABEL}
+                 COMMAND ${name} --gtest_filter=CheckPDBTrajCoordinates.*)
+        # add_test(NAME CheckpointTest_${ENSEMBLE_LABEL}
+        #          COMMAND ${name} --gtest_filter=CheckMollookup.*)
+    endif()
+endfunction()
+
+# Define per-ensemble wrappers
 function(add_NVT_test name)
-      set(NVT_flags "-DENSEMBLE=1")
-      # My test are all called name_test.cpp
-      add_executable(${name} ${GOMCHeaders} ${GOMCSources} ${libHeaders} ${libSources} ${TestHeaders} ${TestSources})
-      target_link_libraries(${name} gtest_main)
-      set_target_properties(${name} PROPERTIES 
-      COMPILE_FLAGS "${NVT_flags}")
-      add_test(NAME BasicTypesTest_NVT COMMAND BasicTypesTest)
-      #add_test(NAME CircuitTester_NVT COMMAND DialaTest)
-      add_test(NAME MolLookupTest_NVT COMMAND CheckConsensusBeta)
-      #add_test(NAME PSFParserTest_NVT COMMAND CheckProtAndWaterTest)
-endfunction(add_NVT_test)
+    add_ensemble_test(${name} 1 NVT)
+endfunction()
 
 function(add_NPT_test name)
-      set(NPT_flags "-DENSEMBLE=4")
-      # My test are all called name_test.cpp
-      add_executable(${name} ${GOMCHeaders} ${GOMCSources} ${libHeaders} ${libSources} ${TestHeaders} ${TestSources})
-      target_link_libraries(${name} gtest_main)
-      set_target_properties(${name} PROPERTIES 
-      COMPILE_FLAGS "${NPT_flags}")
-      add_test(NAME BasicTypesTest_NPT COMMAND BasicTypesTest)
-      #add_test(NAME CircuitTester_NPT COMMAND DialaTest)
-      add_test(NAME MolLookupTest_NPT COMMAND CheckConsensusBeta)
-      #add_test(NAME PSFParserTest_NPT COMMAND CheckProtAndWaterTest)
-endfunction(add_NPT_test)
+    add_ensemble_test(${name} 4 NPT)
+endfunction()
 
 function(add_GCMC_test name)
-      set(GCMC_flags "-DENSEMBLE=3")
-      # My test are all called name_test.cpp
-      add_executable(${name} ${GOMCHeaders} ${GOMCSources} ${libHeaders} ${libSources} ${TestHeaders} ${TestSources})
-      target_link_libraries(${name} gtest_main)
-      set_target_properties(${name} PROPERTIES 
-      COMPILE_FLAGS "${GCMC_flags}")
-      add_test(NAME BasicTypesTest_GCMC COMMAND BasicTypesTest)
-      #add_test(NAME CircuitTester_GCMC COMMAND DialaTest)
-      add_test(NAME MolLookupTest_GCMC COMMAND CheckConsensusBeta)
-      #add_test(NAME PSFParserTest_GCMC COMMAND CheckProtAndWaterTest)
-      add_test(NAME ConsistentTrajectoryTest_GCMC COMMAND CheckPDBTrajCoordinates)
-      #add_test(NAME CheckpointTest_GCMC COMMAND CheckMollookup)
-endfunction(add_GCMC_test)
+    add_ensemble_test(${name} 3 GCMC)
+endfunction()
 
 function(add_GEMC_test name)
-      set(GEMC_flags "-DENSEMBLE=2")
-      # My test are all called name_test.cpp
-      add_executable(${name} ${GOMCHeaders} ${GOMCSources} ${libHeaders} ${libSources} ${TestHeaders} ${TestSources})
-      target_link_libraries(${name} gtest_main)
-      set_target_properties(${name} PROPERTIES 
-      COMPILE_FLAGS "${GEMC_flags}")
-      add_test(NAME BasicTypesTest_GEMC COMMAND BasicTypesTest)
-      #add_test(NAME CircuitTester_GEMC COMMAND DialaTest)
-      add_test(NAME MolLookupTest_GEMC COMMAND CheckConsensusBeta)
-      #add_test(NAME PSFParserTest_GEMC COMMAND CheckProtAndWaterTest)
-      add_test(NAME ConsistentTrajectoryTest_GEMC COMMAND CheckPDBTrajCoordinates)
-      #add_test(NAME CheckpointTest_GEMC COMMAND CheckMollookup)
-endfunction(add_GEMC_test)
+    add_ensemble_test(${name} 2 GEMC)
+endfunction()
 
+# Register all test executables
 add_NVT_test(GOMC_NVT_Test)
 add_NPT_test(GOMC_NPT_Test)
 add_GCMC_test(GOMC_GCMC_Test)
